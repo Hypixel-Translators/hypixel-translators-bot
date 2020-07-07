17e6d780cca7a491f4a29ddf75a2575dec827860
QkeleQ10 <quintenra@gmail.com>
2020-07-07 10:10:31 +0200

    added cache to match V12

--- commands/prefix.js
+++ commands/prefix.js
@@ -26,132 +26,132 @@ module.exports = {
           const prefixes = ""
           const guild = msg.client.guilds.cache.get("549503328472530974")
           const user = guild.member(args[0])
-          if (user.roles.some(r => r.name.startsWith("Bulgarian"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Bulgarian"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇧🇬")
           }
-          if (user.roles.some(r => r.name.startsWith("Chinese"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Chinese"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇨🇳")
           }
-          if (user.roles.some(r => r.name.startsWith("Czech"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Czech"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇨🇿")
           }
-          if (user.roles.some(r => r.name.startsWith("Danish"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Danish"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇩🇰")
           }
-          if (user.roles.some(r => r.name.startsWith("Dutch"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Dutch"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇳🇱")
           }
-          if (user.roles.some(r => r.name.startsWith("Finnish"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Finnish"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇫🇮")
           }
-          if (user.roles.some(r => r.name.startsWith("French"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("French"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇫🇷")
           }
-          if (user.roles.some(r => r.name.startsWith("German"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("German"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇩🇪")
-          } if (user.roles.some(r => r.name.startsWith("Greek"))) {
+          } if (user.roles.cache.some(r => r.name.startsWith("Greek"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇬🇷")
           }
-          if (user.roles.some(r => r.name.startsWith("Italian"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Italian"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇮🇹")
           }
-          if (user.roles.some(r => r.name.startsWith("Japanese"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Japanese"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇯🇵")
           }
-          if (user.roles.some(r => r.name.startsWith("Korean"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Korean"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇰🇷")
           }
-          if (user.roles.some(r => r.name.startsWith("Norwegian"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Norwegian"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇳🇴")
           }
-          if (user.roles.some(r => r.name.startsWith("Polish"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Polish"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇵🇱")
           }
-          if (user.roles.some(r => r.name.startsWith("Portuguese"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Portuguese"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇵🇹")
           }
-          if (user.roles.some(r => r.name.startsWith("Brazilian"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Brazilian"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇧🇷")
           }
-          if (user.roles.some(r => r.name.startsWith("Russian"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Russian"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇷🇺")
           }
-          if (user.roles.some(r => r.name.startsWith("Spanish"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Spanish"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇪🇸")
           }
-          if (user.roles.some(r => r.name.startsWith("Swedish"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Swedish"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇸🇪")
           }
-          if (user.roles.some(r => r.name.startsWith("Thai"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Thai"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇹🇭")
           }
-          if (user.roles.some(r => r.name.startsWith("Turkish"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Turkish"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
             prefixes = (prefixes + "🇹🇷")
           }
-          if (user.roles.some(r => r.name.startsWith("Ukrainian"))) {
+          if (user.roles.cache.some(r => r.name.startsWith("Ukrainian"))) {
             if (prefixes.length > 4) {
               prefixes = (prefixes + "-")
             }
