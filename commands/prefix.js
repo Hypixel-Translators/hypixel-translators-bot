2d102c4699679f9a3f56da34dceef627f7a91c8f
QkeleQ10 <quintenra@gmail.com>
2020-07-07 10:04:17 +0200

    lots of ifs 😩

--- commands/prefix.js
+++ commands/prefix.js
@@ -23,12 +23,140 @@ module.exports = {
     message.channel.send(embed)
       .then(msg => {
         if (args[0]) {
+          const prefixes = ""
           const guild = msg.client.guilds.cache.get("549503328472530974")
           const user = guild.member(args[0])
-          var roles = user.roles.member[0].guild[0]._roles
-          console.log(roles)
-          var langRoles = roles.filter(x => !x.name.endsWith("Proofreader") || !x.name.endsWith("Translator"))
-          console.log(langRoles)
+          if (user.roles.find(r => r.name.startsWith("Bulgarian"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇧🇬")
+          }
+          if (user.roles.find(r => r.name.startsWith("Chinese"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇨🇳")
+          }
+          if (user.roles.find(r => r.name.startsWith("Czech"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇨🇿")
+          }
+          if (user.roles.find(r => r.name.startsWith("Danish"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇩🇰")
+          }
+          if (user.roles.find(r => r.name.startsWith("Dutch"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇳🇱")
+          }
+          if (user.roles.find(r => r.name.startsWith("Finnish"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇫🇮")
+          }
+          if (user.roles.find(r => r.name.startsWith("French"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇫🇷")
+          }
+          if (user.roles.find(r => r.name.startsWith("German"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇩🇪")
+          } if (user.roles.find(r => r.name.startsWith("Greek"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇬🇷")
+          }
+          if (user.roles.find(r => r.name.startsWith("Italian"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇮🇹")
+          }
+          if (user.roles.find(r => r.name.startsWith("Japanese"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇯🇵")
+          }
+          if (user.roles.find(r => r.name.startsWith("Korean"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇰🇷")
+          }
+          if (user.roles.find(r => r.name.startsWith("Norwegian"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇳🇴")
+          }
+          if (user.roles.find(r => r.name.startsWith("Polish"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇵🇱")
+          }
+          if (user.roles.find(r => r.name.startsWith("Portuguese"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇵🇹")
+          }
+          if (user.roles.find(r => r.name.startsWith("Brazilian"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇧🇷")
+          }
+          if (user.roles.find(r => r.name.startsWith("Russian"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇷🇺")
+          }
+          if (user.roles.find(r => r.name.startsWith("Spanish"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇪🇸")
+          }
+          if (user.roles.find(r => r.name.startsWith("Swedish"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇸🇪")
+          }
+          if (user.roles.find(r => r.name.startsWith("Thai"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇹🇭")
+          }
+          if (user.roles.find(r => r.name.startsWith("Turkish"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇹🇷")
+          }
+          if (user.roles.find(r => r.name.startsWith("Ukrainian"))) {
+            if (prefixes.length > 4) {
+              prefixes = (prefixes + "-")
+            }
+            prefixes = (prefixes + "🇺🇦")
+          }
         }
 
 
@@ -36,7 +164,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
           .setColor(errorColor)
           .setTitle("Prefix")
-          .setDescription("This command hasn't been programmed yet, this is a fallback message. ")
+          .setDescription("This command hasn't been programmed yet, this is a fallback message.")
           .setFooter("Executed by " + message.author.tag);
         msg.edit(embed)
       })
