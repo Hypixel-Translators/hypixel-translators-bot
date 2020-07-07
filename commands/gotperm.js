94fe1e702e77f35f8706dc631aed3b1f68c7cccd
QkeleQ10 <quintenra@gmail.com>
2020-07-07 10:43:35 +0200

    Update gotperm.js

--- commands/gotperm.js
+++ commands/gotperm.js
@@ -12,20 +12,21 @@ module.exports = {
   usage: "<perms> [user]",
   execute(message, args) {
     //message.delete();
+    const perm = toUpperCase(args[0])
     const embed = new Discord.MessageEmbed()
-      .setTitle("Permission " + toUpperCase(args[0]))
+      .setTitle("Permission " + perm)
       .setDescription("One second...")
       .setFooter("Executed by " + message.author.tag);
     message.channel.send(embed).then(msg => {
-      const authorPerm = message.member.hasPermission(toUpperCase(args[0]));
-      const botPerm = msg.member.hasPermission(toUpperCase(args[0]));
+      const authorPerm = message.member.hasPermission(perm);
+      const botPerm = msg.member.hasPermission(perm);
       if (args[1]) {
         const guild = msg.client.guilds.get("549503328472530974")
         const user = guild.member(args[1])
         console.log(user);
-        const userPerm = user.hasPermission(toUpperCase(args[0]));
+        const userPerm = user.hasPermission(perm);
         const embed = new Discord.MessageEmbed()
-          .setTitle("Permission " + toUpperCase(args[0]))
+          .setTitle("Permission " + perm)
           .addFields(
             { name: "Bot", value: botPerm },
             { name: "Author", value: authorPerm },
@@ -35,7 +36,7 @@ module.exports = {
         msg.edit(embed);
       } else {
         const embed = new Discord.MessageEmbed()
-          .setTitle("Permission " + toUpperCase(toUpperCase(args[0])))
+          .setTitle("Permission " + toUpperCase(perm))
           .addFields(
             { name: "Bot", value: botPerm },
             { name: "Author", value: authorPerm }
