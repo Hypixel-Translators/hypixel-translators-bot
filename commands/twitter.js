53b8f9f8513740d26e1413f4347198cf5b5328c8
QkeleQ10 <quintenra@gmail.com>
2020-07-08 15:49:10 +0200

    Remove weird random space

--- commands/twitter.js
+++ commands/twitter.js
@@ -14,7 +14,7 @@ const {
     channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
     execute(message) {
       //message.delete();
-      message.channel.send("This is our official Twitter account:\nhttps://twitter.com/HTranslators\n Please feel free to share it with others!")
+      message.channel.send("This is our official Twitter account:\nhttps://twitter.com/HTranslators\nPlease feel free to share it with others!")
     }
   };
   
