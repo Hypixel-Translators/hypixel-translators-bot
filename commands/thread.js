df12ef215a793eb62d50a3ae22b34500f9faa5f0
QkeleQ10 <quintenra@gmail.com>
2020-07-08 15:17:25 +0200

    Create thread.js

---
+++ commands/thread.js
@@ -0,0 +1,21 @@
+const {
+    workingColor,
+    errorColor,
+    successColor,
+    neutralColor
+  } = require("../config.json");
+  const Discord = require("discord.js");
+  
+  module.exports = {
+    name: "thread",
+    description: "Gives you the thread regarding this Discord server.",
+    aliases: ["discordthread", "communitythread"],
+    usage: "thread",
+    cooldown: 30,
+    channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
+    execute(message) {
+      //message.delete();
+      message.channel.send("This is the official thread for this community Discord server:\nhttps://hypixel.net/threads/hypixel-translator-discord.1970571")
+    }
+  };
+  
