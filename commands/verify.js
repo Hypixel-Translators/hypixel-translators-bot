28db72f0f5e9f45d6421c2b42bb34352e99db091
QkeleQ10 <quintenra@gmail.com>
2020-07-08 23:30:36 +0200

    verify- safe to merge

--- commands/verify.js
+++ commands/verify.js
@@ -8,8 +8,25 @@ module.exports = {
     cooldown: 3,
     execute(message) {
         if (message.member.hasPermission("ADMINISTRATOR")) {
+            var userToSend = args[0].replace(/[\\<>@#&!]/g, "");
+            var type = args[1]
+            const lowerArg = args[0].toLowerCase()
+            var toLook = lowerArg.charAt(0).toUpperCase() + lowerArg.slice(1)
+
             //message.delete();
-            message.channel.send("Not set up yet.")
+            const embed = new Discord.MessageEmbed()
+                .setColor(workingColor)
+                .setTitle("Verify")
+                .setDescription("One second... ")
+                .addFields(
+                    { name: "Message", value: toSend },
+                    { name: "Recipient", value: "<@" + userToSend + ">" }
+                )
+                .setFooter("Executed by " + message.author.tag);
+            message.channel.send(embed)
+                .then(msg => {
+                    const recipient = msg.client.users.cache.get(userToSend)
+                })
         }
     }
 };
