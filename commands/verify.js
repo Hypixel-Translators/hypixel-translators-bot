ccd4c3b57bfe9e4a50c79ecbe3f73105d401a95d
QkeleQ10 <quintenra@gmail.com>
2020-07-08 22:12:21 +0200

    only admin dm command

--- commands/verify.js
+++ commands/verify.js
@@ -5,10 +5,11 @@ module.exports = {
     name: "verify",
     description: "Verifies the user on the Hypixel project.",
     usage: "verify <mention> <language> <role> <profile>",
-    cooldown: 30,
-    channelWhiteList: ["569178590697095168"],
+    cooldown: 3,
     execute(message) {
-        //message.delete();
-        message.channel.send("Not set up yet.")
+        if (message.member.hasPermission("ADMINISTRATOR")) {
+            //message.delete();
+            message.channel.send("Not set up yet.")
+        }
     }
 };
