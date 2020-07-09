16529f680ea9f53aeffbcbf174e0e82ba70633ad
QkeleQ10 <quintenra@gmail.com>
2020-07-09 13:40:12 +0200

    just some console logging

--- index.js
+++ index.js
@@ -25,13 +25,15 @@ client.once("ready", () => {
   client.user.setStatus("online").catch(console.error);
   setInterval(() => {
     var guild = client.guilds.cache.get("549503328472530974")
-    var randomUser = guild.members.cache.random()
+    var randomUser = guild.members.cache.random(); console.log("Looked for user");
+
     while (isTooOld) {
       if (randomUser.lastMessage) {
         var isTooOld = (now - randomUser.lastMessage.createdAt) > tooOld
-      } else { var randomUser = guild.members.cache.random() }
-      if (isTooOld) { var randomUser = guild.members.cache.random() }
+      } else { var randomUser = guild.members.cache.random(); console.log("OldMsg, Looked for another user"); }
+      if (isTooOld) { var randomUser = guild.members.cache.random(); console.log("NoMsgs, Looked for another user"); }
     }
+
     var randomUserName = randomUser.user.username
     if (used1) {
       var listenStatus = listenStatuses[Math.floor(Math.random() * listenStatuses.length)]
