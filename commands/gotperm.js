bef81f3275cd5626e50f18beea6df1f16cd742eb
QkeleQ10 <quintenra@gmail.com>
2020-07-12 22:23:19 +0200

    Add color to gotperm

--- commands/gotperm.js
+++ commands/gotperm.js
@@ -16,18 +16,22 @@ module.exports = {
     //message.delete();
     const perm = args[0].toUpperCase()
     const embed = new Discord.MessageEmbed()
+      .setColor(workingColor)
       .setTitle("Permission " + perm)
       .setDescription("One second...")
       .setFooter("Executed by " + message.author.tag);
     message.channel.send(embed).then(msg => {
       const authorPerm = message.member.hasPermission(perm);
       const botPerm = msg.member.hasPermission(perm);
+
       if (args[1]) {
         const guild = msg.client.guilds.get("549503328472530974")
         const user = guild.member(args[1])
         console.log(user);
         const userPerm = user.hasPermission(perm);
+
         const embed = new Discord.MessageEmbed()
+          .setColor(successColor)
           .setTitle("Permission " + perm)
           .addFields(
             { name: "Bot", value: botPerm },
@@ -36,8 +40,11 @@ module.exports = {
           )
           .setFooter("Executed by " + message.author.tag);
         msg.edit(embed);
+
       } else {
+
         const embed = new Discord.MessageEmbed()
+          .setColor(successColor)
           .setTitle("Permission " + perm)
           .addFields(
             { name: "Bot", value: botPerm },
