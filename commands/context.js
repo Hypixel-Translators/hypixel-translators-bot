d017840bf834e3cf148ebd1c03612dd41fe23cf3
QkeleQ10 <quintenra@gmail.com>
2020-07-12 16:37:06 +0200

    whitelist!

--- commands/context.js
+++ commands/context.js
@@ -7,6 +7,7 @@ module.exports = {
     name: "context",
     description: "Does nothing for now!",
     usage: "context <link/ID>",
+    channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
     cooldown: 3,
     execute(message, args) {
         const req = ContextModel.findOne({ id: args[0] })
