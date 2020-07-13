d57b948827fd560a64fed6a506f259fcc0f5f914
QkeleQ10 <quintenra@gmail.com>
2020-07-13 14:35:49 +0200

    Disable mongoose implementation to make room for google sheets

--- commands/context.js
+++ commands/context.js
@@ -1,7 +1,5 @@
 const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
 const Discord = require("discord.js");
-const ContextModel = require('../models/Context')
-const { connect } = require('mongoose')
 
 module.exports = {
     name: "context",
@@ -10,56 +8,6 @@ module.exports = {
     channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
     cooldown: 3,
     execute(message, args) {
-        const req = ContextModel.findOne({ id: args[0] })
 
-        console.log(message + "\n" + args)
-        console.log(req)
-        if (!req) {
-            const embed = new Discord.MessageEmbed()
-                .setColor(neutralColor)
-                .setTitle("Context")
-                .setDescription("This string wasn't found. To create a new context entry, react with ✅ within 10 seconds.")
-                .setFooter("Executed by " + message.author.tag);
-            message.channel.send(embed).then(msg => {
-                msg.react("✅")
-
-                const filter = (reaction, reacter) => {
-                    return (reaction.emoji.name === "✅") && reacter.id === message.author.id;
-                };
-
-                const collector = msg.createReactionCollector(filter, { time: 10000 });
-
-                collector.on('collect', (reaction, reacter) => {
-                    if (reaction.emoji.name === "✅") {
-                        const doc = new ContextModel({ id: args[0] })
-                        const embed = new Discord.MessageEmbed()
-                            .setColor(successColor)
-                            .setTitle("Context")
-                            .setDescription("A new context entry has been made!\n\n> " + doc)
-                            .setFooter("Executed by " + message.author.tag);
-                        msg.edit(embed)
-                    }
-                })
-
-                collector.on('end', collected => {
-                    if (!collected) {
-                        const embed = new Discord.MessageEmbed()
-                            .setColor(neutralColor)
-                            .setTitle("Context")
-                            .setDescription("This string wasn't found. To create a new context entry, re-run this command.")
-                            .setFooter("Executed by " + message.author.tag);
-                        msg.edit(embed)
-                    }
-                })
-
-            })
-        } else {
-            const embed = new Discord.MessageEmbed()
-                .setColor(successColor)
-                .setTitle("Context")
-                .setDescription("This entry has been found!\n\n> " + req)
-                .setFooter("Executed by " + message.author.tag);
-            message.channel.send(embed)
-        }
     }
 }
