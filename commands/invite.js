10063e4cfbd74b9044bb7f37104c0f9b74582d16
QkeleQ10 <quintenra@gmail.com>
2020-07-13 20:04:10 +0200

    Enable +invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,20 +1,15 @@
-/*const {
-    workingColor,
-    errorColor,
-    successColor,
-    neutralColor
-  } = require("../config.json");
-  const Discord = require("discord.js");
-  
-  module.exports = {
-    name: "invite",
-    description: "Gives you the server's invite link.",
-    aliases: ["invitelink"],
-    usage: "invite",
-    cooldown: 30,
-    channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
-    execute(message) {
-      //message.delete();
-      message.channel.send("You can use this link to invite others to the community:\nhttps://discord.gg/rcT948A")
-    }
-  };*/
+const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  description: "Gives you the server's invite link.",
+  aliases: ["invitelink"],
+  usage: "invite",
+  cooldown: 30,
+  channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
+  execute(message) {
+    //message.delete();
+    message.channel.send("You can use this link to invite others to the community:\nhttps://discord.gg/rcT948A")
+  }
+};
