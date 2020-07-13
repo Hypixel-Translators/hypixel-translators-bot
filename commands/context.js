419ef7a039f2e016911811407215780545d1944f
QkeleQ10 <quintenra@gmail.com>
2020-07-13 15:53:46 +0200

    Update context.js

--- commands/context.js
+++ commands/context.js
@@ -1,6 +1,8 @@
 const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
 const Discord = require("discord.js");
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const { promisify } = require('util')
+const credentials = require('../.gitignore/service-account.json')
 
 
 module.exports = {
@@ -10,19 +12,17 @@ module.exports = {
     channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
     cooldown: 3,
     execute(message, args) {
-        const doc = new GoogleSpreadsheet('1tVLWskn4InBeopmRdQyrDumr1H6STqyidcEwoL4a8ts');
-        doc.useServiceAccountAuth({
-            client_email: 'database@hypixel-translators-bot.iam.gserviceaccount.com',
-            private_key: '8f8057b93cce4dda659f117b0401582414e10637',
-        })
-            .then(() => {
-                doc.loadInfo()
-                    .then(() => {
-                        const sheet = doc.sheetsByIndex[0]
-                        console.log(sheet.title)
-                    })
-                    .catch(err => { console.log(err); })
-            })
-            .catch(err => { console.log(err); })
+
+
     }
+}
+
+const SPREADSHEET_ID = '8f8057b93cce4dda659f117b0401582414e10637'
+async function accessSpreadsheet() {
+    const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
+    await promisify(doc.useServiceAccountAuth)(creds)
+    const info = await promisify(doc.getInfo)()
+    console.log('Found document: ' + info.title + ' by ' + info.author.email)
+    const sheet = info.worksheets[0]
+    console.log('sheet 1: ' + sheet.title + ' with rows: ' + sheet.rowCount + " and columns: " + sheet.colCount)
 }
