a3ea323724d81048a6d985d1b3238639102b308d
QkeleQ10 <quintenra@gmail.com>
2020-07-13 14:40:27 +0200

    log sheet title on +context

--- commands/context.js
+++ commands/context.js
@@ -1,5 +1,7 @@
 const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
 const Discord = require("discord.js");
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
 
 module.exports = {
     name: "context",
@@ -8,6 +10,10 @@ module.exports = {
     channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
     cooldown: 3,
     execute(message, args) {
-
+        const doc = new GoogleSpreadsheet('1tVLWskn4InBeopmRdQyrDumr1H6STqyidcEwoL4a8ts');
+        doc.useServiceAccountAuth({
+            client_email: 'database@hypixel-translators-bot.iam.gserviceaccount.com',
+            private_key: '8f8057b93cce4dda659f117b0401582414e10637',
+        }).then(() => { doc.loadInfo().then(() => { console.log(doc.title) }) })
     }
 }
