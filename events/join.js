f0e0fb81d4be932178ccce6470ddfcec38150baf
QkeleQ10 <quintenra@gmail.com>
2020-07-14 15:02:27 +0200

    Update join.js

--- events/join.js
+++ events/join.js
@@ -2,11 +2,6 @@ const { workingColor, errorColor, successColor, neutralColor } = require("../con
 const Discord = require("discord.js");
 
 module.exports = {
-    name: "context",
-    description: "Does nothing for now!",
-    usage: "context <link/ID>",
-    channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
-    cooldown: 3,
     execute(member) {
         const embed = new Discord.MessageEmbed()
             .setColor(neutralColor)
