075002ad47935caace2031c96681840632c75265
QkeleQ10 <quintenra@gmail.com>
2020-07-15 21:04:53 +0200

    role menu prototype

---
+++ commands/reactionmenu.js
@@ -0,0 +1,25 @@
+const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "rolemenu",
+    description: "Create a new reaction menu. This isn't immediately functional.",
+    usage: "rolemenu",
+    cooldown: 3,
+    execute(message, args) {
+        message.delete();
+        var allowed = false
+        if (message.author.id == "722738307477536778") { allowed = true }
+        if (message.channel.type !== "dm") { if (message.member.roles.cache.has("621071221462663169") || message.member.roles.cache.has("549885657749913621") || message.member.roles.cache.has("241926666400563203")) { allowed = true } }
+        if (!allowed) return;
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(neutralColor)
+            .setTitle("Get notified of bot updates")
+            .setDescription("React with 🤖 to get mentioned whenever a bot update comes out.")
+            .setFooter("This menu is not fully operational yet, wait about a minute for the changes to come through.");
+        message.channel.send(embed).then(msg => {
+            msg.react("🤖")
+        })
+    }
+};
---
+++ events/reactionAdd.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token, workingColor, errorColor, successColor, neutralColor } = require("./config.json");
+
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.log('Something went wrong when fetching the message: ', error);
+            return;
+        }
+    }
+
+    if (reaction.message.id === "") {
+        const role = reaction.message.guild.roles.cache.get("732615152246980628") //Bot Updates
+        user.roles.add(role)
+            .then(() => {
+                const receivedEmbed = message.embeds[0];
+                const embed = new Discord.MessageEmbed(receivedEmbed)
+                    .setFooter('Given ' + user.username + " the Bot Updates role!")
+                    .setColor(successColor)
+                reaction.message.edit(embed)
+                setInterval(() => {
+                    embed
+                        .setFooter("If this text doesn't change to a confirming message after you've reacted, please contact QkeleQ10#6046.")
+                        .setColor(neutralColor)
+                    reaction.message.edit(embed)
+                }, 5000)
+            })
+            .catch(err => {
+                const receivedEmbed = message.embeds[0];
+                const embed = new Discord.MessageEmbed(receivedEmbed)
+                    .setFooter("Something went wrong, please contact QkeleQ10#6046")
+                    .setColor(errorColor)
+                reaction.message.edit(embed)
+                setInterval(() => {
+                    embed
+                        .setFooter("If this text doesn't change to a confirming message after you've reacted, please contact QkeleQ10#6046.")
+                        .setColor(neutralColor)
+                    reaction.message.edit(embed)
+                }, 5000)
+            })
+    }
+});
