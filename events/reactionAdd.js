148a7af0bbe44c1401ac5964507fcee22bdb2360
QkeleQ10 <quintenra@gmail.com>
2020-07-15 21:08:09 +0200

    link

--- events/reactionAdd.js
+++ events/reactionAdd.js
@@ -15,13 +15,55 @@ client.on('messageReactionAdd', async (reaction, user) => {
         }
     }
 
-    if (reaction.message.id === "") {
+    if (reaction.message.id === "733036798736990309") {
         const role = reaction.message.guild.roles.cache.get("732615152246980628") //Bot Updates
         user.roles.add(role)
             .then(() => {
                 const receivedEmbed = message.embeds[0];
                 const embed = new Discord.MessageEmbed(receivedEmbed)
                     .setFooter('Given ' + user.username + " the Bot Updates role!")
+                    .setColor(successColor)
+                reaction.message.edit(embed)
+                setInterval(() => {
+                    embed
+                        .setFooter("If this text doesn't change to a confirming message after you've reacted, please contact QkeleQ10#6046.")
+                        .setColor(neutralColor)
+                    reaction.message.edit(embed)
+                }, 5000)
+            })
+            .catch(err => {
+                const receivedEmbed = message.embeds[0];
+                const embed = new Discord.MessageEmbed(receivedEmbed)
+                    .setFooter("Something went wrong, please contact QkeleQ10#6046")
+                    .setColor(errorColor)
+                reaction.message.edit(embed)
+                setInterval(() => {
+                    embed
+                        .setFooter("If this text doesn't change to a confirming message after you've reacted, please contact QkeleQ10#6046.")
+                        .setColor(neutralColor)
+                    reaction.message.edit(embed)
+                }, 5000)
+            })
+    }
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.log('Something went wrong when fetching the message: ', error);
+            return;
+        }
+    }
+
+    if (reaction.message.id === "733036798736990309") {
+        const role = reaction.message.guild.roles.cache.get("732615152246980628") //Bot Updates
+        user.roles.remove(role)
+            .then(() => {
+                const receivedEmbed = message.embeds[0];
+                const embed = new Discord.MessageEmbed(receivedEmbed)
+                    .setFooter('Removed ' + user.username + "'s Bot Updates role!")
                     .setColor(successColor)
                 reaction.message.edit(embed)
                 setInterval(() => {
