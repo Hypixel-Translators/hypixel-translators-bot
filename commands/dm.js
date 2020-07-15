4d7fd4731b368629290dc29e052521d067c596de
QkeleQ10 <quintenra@gmail.com>
2020-07-15 17:13:55 +0200

    a

--- commands/dm.js
+++ commands/dm.js
@@ -11,7 +11,7 @@ module.exports = {
     execute(message, args) {
         var allowed = false
         if (message.author.id == "722738307477536778") { allowed = true }
-        if (message.member) { if (message.member.roles.cache.has("621071221462663169") || message.member.roles.cache.has("549885657749913621") || message.member.roles.cache.has("241926666400563203")) { allowed = true } }
+        if (message.channel.type !== "dm") { if (message.member.roles.cache.has("621071221462663169") || message.member.roles.cache.has("549885657749913621") || message.member.roles.cache.has("241926666400563203")) { allowed = true } }
         if (!allowed) return;
 
         var userToSend = args[0].replace(/[\\<>@#&!]/g, "");
--- commands/say.js
+++ commands/say.js
@@ -18,23 +18,26 @@ module.exports = {
     var toSend = args.join(" ")
 
     //message.delete();
-    if (message.author.id == "722738307477536778") {
-      const embed = new Discord.MessageEmbed()
-        .setColor(workingColor)
-        .setTitle("Say")
-        .setDescription("Saying...")
-        .setFooter("Executed by " + message.author.tag);
-      message.channel.send(embed)
-        .then(msg => {
-          const sendTo = msg.client.channels.cache.get(rawSendTo)
-          sendTo.send(toSend)
-          const embed = new Discord.MessageEmbed()
-            .setColor(successColor)
-            .setTitle("Say")
-            .setDescription("Said!")
-            .setFooter("Executed by " + message.author.tag);
-          msg.edit(embed)
-        })
-    }
+    var allowed = false
+    if (message.author.id == "722738307477536778") { allowed = true }
+    if (message.channel.type !== "dm") { if (message.member.roles.cache.has("621071221462663169") || message.member.roles.cache.has("549885657749913621") || message.member.roles.cache.has("241926666400563203")) { allowed = true } }
+    if (!allowed) return;
+
+    const embed = new Discord.MessageEmbed()
+      .setColor(workingColor)
+      .setTitle("Say")
+      .setDescription("Saying...")
+      .setFooter("Executed by " + message.author.tag);
+    message.channel.send(embed)
+      .then(msg => {
+        const sendTo = msg.client.channels.cache.get(rawSendTo)
+        sendTo.send(toSend)
+        const embed = new Discord.MessageEmbed()
+          .setColor(successColor)
+          .setTitle("Say")
+          .setDescription("Said!")
+          .setFooter("Executed by " + message.author.tag);
+        msg.edit(embed)
+      })
   }
 };
