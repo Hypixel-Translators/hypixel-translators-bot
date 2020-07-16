983ebd4d3525730a7afab92488145dcfa90292ae
QkeleQ10 <quintenra@gmail.com>
2020-07-16 20:20:33 +0200

    Update context.js

--- commands/context.js
+++ commands/context.js
@@ -7,7 +7,7 @@ const creds = require('../service-account.json')
 module.exports = {
     name: "context",
     description: "",
-    usage: "context <string ID> [language code]",
+    usage: "context <string ID> []",
     categoryBlackList: ["549503328472530975"],
     cooldown: 3,
     execute(message, args) {
@@ -47,32 +47,34 @@ async function accessSpreadsheet(message, args, msg) {
             { name: "Context", value: correctRow.context }
         )
         .setFooter("Executed by " + message.author.tag);
-    if (correctRow.screenshot) { embed.setImage(correctRow.screenshot) }
+    if (correctRow) {
+        if (correctRow.screenshot) { embed.setImage(correctRow.screenshot) }
 
-    if (correctRow.bg.length > 1) { embed.addFields({ name: "Note for Bulgarian", value: correctRow.bg }) }
-    if (correctRow.zhCN.length > 1) { embed.addFields({ name: "Note for Chinese (Simplified)", value: correctRow.zhCN }) }
-    if (correctRow.zhTW.length > 1) { embed.addFields({ name: "Note for Chinese (Traditional)", value: correctRow.zhTW }) }
-    if (correctRow.cs.length > 1) { embed.addFields({ name: "Note for Czech", value: correctRow.cs }) }
-    if (correctRow.da.length > 1) { embed.addFields({ name: "Note for Danish", value: correctRow.da }) }
-    if (correctRow.nl.length > 1) { embed.addFields({ name: "Note for Dutch", value: correctRow.nl }) }
-    if (correctRow.fi.length > 1) { embed.addFields({ name: "Note for Finnish", value: correctRow.fi }) }
-    if (correctRow.fr.length > 1) { embed.addFields({ name: "Note for French", value: correctRow.fr }) }
-    if (correctRow.de.length > 1) { embed.addFields({ name: "Note for German", value: correctRow.de }) }
-    if (correctRow.el.length > 1) { embed.addFields({ name: "Note for Greek", value: correctRow.el }) }
-    if (correctRow.it.length > 1) { embed.addFields({ name: "Note for Italian", value: correctRow.it }) }
-    if (correctRow.ja.length > 1) { embed.addFields({ name: "Note for Japanese", value: correctRow.ja }) }
-    if (correctRow.ko.length > 1) { embed.addFields({ name: "Note for Korean", value: correctRow.ko }) }
-    if (correctRow.no.length > 1) { embed.addFields({ name: "Note for Norwegian", value: correctRow.no }) }
-    if (correctRow.enPT.length > 1) { embed.addFields({ name: "Note fer Pirate ☠️", value: correctRow.enPT }) }
-    if (correctRow.pl.length > 1) { embed.addFields({ name: "Note for Polish", value: correctRow.pl }) }
-    if (correctRow.ptPT.length > 1) { embed.addFields({ name: "Note for Portuguese", value: correctRow.ptPT }) }
-    if (correctRow.ptBR.length > 1) { embed.addFields({ name: "Note for Brazilian", value: correctRow.ptBR }) }
-    if (correctRow.ru.length > 1) { embed.addFields({ name: "Note for Russian", value: correctRow.ru }) }
-    if (correctRow.esES.length > 1) { embed.addFields({ name: "Note for Spanish", value: correctRow.esES }) }
-    if (correctRow.svSE.length > 1) { embed.addFields({ name: "Note for Swedish", value: correctRow.svSE }) }
-    if (correctRow.th.length > 1) { embed.addFields({ name: "Note for Thai", value: correctRow.th }) }
-    if (correctRow.tr.length > 1) { embed.addFields({ name: "Note for Turkish", value: correctRow.tr }) }
-    if (correctRow.uk.length > 1) { embed.addFields({ name: "Note for Ukrainian", value: correctRow.uk }) }
+        if (correctRow.bg) { if (correctRow.bg.length > 1) { embed.addFields({ name: "Note for Bulgarian", value: correctRow.bg }) } }
+        if (correctRow.zhCN) { if (correctRow.zhCN.length > 1) { embed.addFields({ name: "Note for Chinese (Simplified)", value: correctRow.zhCN }) } }
+        if (correctRow.zhTW) { if (correctRow.zhTW.length > 1) { embed.addFields({ name: "Note for Chinese (Traditional)", value: correctRow.zhTW }) } }
+        if (correctRow.cs) { if (correctRow.cs.length > 1) { embed.addFields({ name: "Note for Czech", value: correctRow.cs }) } }
+        if (correctRow.da) { if (correctRow.da.length > 1) { embed.addFields({ name: "Note for Danish", value: correctRow.da }) } }
+        if (correctRow.nl) { if (correctRow.nl.length > 1) { embed.addFields({ name: "Note for Dutch", value: correctRow.nl }) } }
+        if (correctRow.fi) { if (correctRow.fi.length > 1) { embed.addFields({ name: "Note for Finnish", value: correctRow.fi }) } }
+        if (correctRow.fr) { if (correctRow.fr.length > 1) { embed.addFields({ name: "Note for French", value: correctRow.fr }) } }
+        if (correctRow.de) { if (correctRow.de.length > 1) { embed.addFields({ name: "Note for German", value: correctRow.de }) } }
+        if (correctRow.el) { if (correctRow.el.length > 1) { embed.addFields({ name: "Note for Greek", value: correctRow.el }) } }
+        if (correctRow.it) { if (correctRow.it.length > 1) { embed.addFields({ name: "Note for Italian", value: correctRow.it }) } }
+        if (correctRow.ja) { if (correctRow.ja.length > 1) { embed.addFields({ name: "Note for Japanese", value: correctRow.ja }) } }
+        if (correctRow.ko) { if (correctRow.ko.length > 1) { embed.addFields({ name: "Note for Korean", value: correctRow.ko }) } }
+        if (correctRow.no) { if (correctRow.no.length > 1) { embed.addFields({ name: "Note for Norwegian", value: correctRow.no }) } }
+        if (correctRow.enPT) { if (correctRow.enPT.length > 1) { embed.addFields({ name: "Note fer Pirate ☠️", value: correctRow.enPT }) } }
+        if (correctRow.pl) { if (correctRow.pl.length > 1) { embed.addFields({ name: "Note for Polish", value: correctRow.pl }) } }
+        if (correctRow.ptPT) { if (correctRow.ptPT.length > 1) { embed.addFields({ name: "Note for Portuguese", value: correctRow.ptPT }) } }
+        if (correctRow.ptBR) { if (correctRow.ptBR.length > 1) { embed.addFields({ name: "Note for Brazilian", value: correctRow.ptBR }) } }
+        if (correctRow.ru) { if (correctRow.ru.length > 1) { embed.addFields({ name: "Note for Russian", value: correctRow.ru }) } }
+        if (correctRow.esES) { if (correctRow.esES.length > 1) { embed.addFields({ name: "Note for Spanish", value: correctRow.esES }) } }
+        if (correctRow.svSE) { if (correctRow.svSE.length > 1) { embed.addFields({ name: "Note for Swedish", value: correctRow.svSE }) } }
+        if (correctRow.th) { if (correctRow.th.length > 1) { embed.addFields({ name: "Note for Thai", value: correctRow.th }) } }
+        if (correctRow.tr) { if (correctRow.tr.length > 1) { embed.addFields({ name: "Note for Turkish", value: correctRow.tr }) } }
+        if (correctRow.uk) { if (correctRow.uk.length > 1) { embed.addFields({ name: "Note for Ukrainian", value: correctRow.uk }) } }
 
-    msg.edit(embed)
+        msg.edit(embed)
+    }
 }
