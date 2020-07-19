de9b04b1d081480949c16643999b631981fd7aee
Quinten Althues <quintenra@gmail.com>
2020-07-19 20:36:57 +0200

    Update context.js

--- commands/context.js
+++ commands/context.js
@@ -6,7 +6,7 @@ const creds = require('../service-account.json')
 module.exports = {
     name: "context",
     description: "Gets or adds context for the given string ID.",
-    usage: "context <string ID>\n+context add <string ID> <context>",
+    usage: "context get|add <string ID> [context (when adding)]",
     categoryBlackList: ["549503328472530975"],
     cooldown: 3,
     execute(message, args) {
@@ -17,12 +17,13 @@ module.exports = {
             .setFooter("Executed by " + message.author.tag);
         message.channel.send(embed)
             .then(msg => {
-                if (args[0] === "new" || args[0] === "add") { addToSpreadsheet(message, args, msg) } else { accessSpreadsheet(message, args, msg) }
+                if (args[0] === "new" || args[0] === "add") { addToSpreadsheet(message, args, msg) }
+                if (args[0] === "get" || args[0] === "add") { getFromSpreadsheet(message, args, msg) }
             })
     }
 }
 
-async function accessSpreadsheet(message, args, msg) {
+async function getFromSpreadsheet(message, args, msg) {
     const doc = new GoogleSpreadsheet('1tVLWskn4InBeopmRdQyrDumr1H6STqyidcEwoL4a8ts')
     await doc.useServiceAccountAuth(creds)
 
@@ -33,7 +34,7 @@ async function accessSpreadsheet(message, args, msg) {
     console.log(sheet.title)
 
     const rows = await sheet.getRows()
-    const correctRow = rows.find(r => r.id === args[0])
+    const correctRow = rows.find(r => r.id === args[1])
 
 
     if (args[1]) {
@@ -41,7 +42,7 @@ async function accessSpreadsheet(message, args, msg) {
             if (!message.member.roles.cache.has("Hypixel Proofreader")) {
                 const embed = new Discord.MessageEmbed()
                     .setColor(errorColor)
-                    .setTitle("Delete context for " + args[0])
+                    .setTitle("Delete context for " + args[1])
                     .setDescription("You can't delete that context entry because you're not a Hypixel Proofreader.")
                     .setFooter("Executed by " + message.author.tag);
                 msg.edit(embed)
@@ -50,7 +51,7 @@ async function accessSpreadsheet(message, args, msg) {
             correctRow.delete()
             const embed = new Discord.MessageEmbed()
                 .setColor(successColor)
-                .setTitle("Delete context for " + args[0])
+                .setTitle("Delete context for " + args[1])
                 .setDescription("The context entry has been deleted!")
                 .setFooter("Executed by " + message.author.tag);
             msg.edit(embed)
@@ -61,7 +62,7 @@ async function accessSpreadsheet(message, args, msg) {
     if (!correctRow) {
         const embed = new Discord.MessageEmbed()
             .setColor(errorColor)
-            .setTitle("Context for " + args[0])
+            .setTitle("Context for " + args[1])
             .setDescription("That context entry hasn't been found.")
             .setFooter("Executed by " + message.author.tag);
         msg.edit(embed)
@@ -70,7 +71,7 @@ async function accessSpreadsheet(message, args, msg) {
 
     const embed = new Discord.MessageEmbed()
         .setColor(successColor)
-        .setTitle("Context for " + args[0])
+        .setTitle("Context for " + args[1])
         .addFields({ name: "Context", value: correctRow.context })
         .setFooter("Executed by " + message.author.tag);
     if (correctRow) {
@@ -106,6 +107,9 @@ async function accessSpreadsheet(message, args, msg) {
 }
 
 async function addToSpreadsheet(message, args, msg) {
+    var toSend = args.splice(0, 2)
+    toSend = toSend.join(" ")
+
     const doc = new GoogleSpreadsheet('1tVLWskn4InBeopmRdQyrDumr1H6STqyidcEwoL4a8ts')
     await doc.useServiceAccountAuth(creds)
 
@@ -118,14 +122,14 @@ async function addToSpreadsheet(message, args, msg) {
     const rows = await sheet.getRows()
 
 
-    var toAdd = { id: args[1], context: args[2] }
+    var toAdd = { id: args[1], context: toSend }
     const embed = new Discord.MessageEmbed()
         .setColor(neutralColor)
-        .setTitle("Add context for " + args[0])
+        .setTitle("Add context for " + args[1])
         .setDescription("The following entry will be added. Please react with 📑 if you'd like to add more to the entry or change existing fields (such as a screenshot or a language note). React with ✅ to submit. This will be cancelled in two minutes.")
         .addFields(
             { name: "String ID", value: args[1] },
-            { name: "Context", value: args[2] }
+            { name: "Context", value: toSend }
         )
         .setFooter("Executed by " + message.author.tag);
     msg.edit(embed).then(msg => {
@@ -139,11 +143,11 @@ async function addToSpreadsheet(message, args, msg) {
 
             collector.on('collect', async (reaction, reacter) => {
                 if (reaction.emoji.name === "📑") {
-                    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+                    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 60000 });
                     const extraEmbed = new Discord.MessageEmbed()
                         .setColor(neutralColor)
-                        .setTitle("Add more to context for " + args[0])
-                        .setDescription("Send `screenshot <image link>` to add a screenshot, or send `<language code (e.g. enPT)> <note>` to add a language note. You can also edit existing fields using `<id|context> <new value`.")
+                        .setTitle("Add more to context for " + args[1])
+                        .setDescription("Send `screenshot <image link>` to add a screenshot, or send `<language code (e.g. enPT)> <note>` to add a language note. You can also edit existing fields using `<id|context> <new value`. You have one minute.")
                     msg.channel.send(extraEmbed).then(extraMsg => {
 
                         collector.on('collect', received => {
@@ -154,11 +158,21 @@ async function addToSpreadsheet(message, args, msg) {
                             toAdd[key] = value
                             const extraEmbed = new Discord.MessageEmbed()
                                 .setColor(successColor)
-                                .setTitle("Add more to context for " + args[0])
+                                .setTitle("Add more to context for " + args[1])
                                 .setDescription("Added this information to the context entry:")
                                 .addFields({ name: key, value: value })
                             extraMsg.edit(extraEmbed)
                         })
+
+                        collector.on('end'), collected => {
+                            if (!collected) {
+                                const extraEmbed = new Discord.MessageEmbed()
+                                    .setColor(errorColor)
+                                    .setTitle("Add more to context for " + args[1])
+                                    .setDescription("You didn't reply in time, so this prompt has been cancelled.")
+                                extraMsg.edit(extraEmbed)
+                            }
+                        }
 
                     })
                 }
@@ -166,40 +180,21 @@ async function addToSpreadsheet(message, args, msg) {
                     const result = await sheet.addRow(toAdd)
                     const embed = new Discord.MessageEmbed()
                         .setColor(successColor)
-                        .setTitle("Add context for " + args[0])
-                        .setDescription("The following entry will be added. Please react with 📑 if you'd like to add more to the entry (such as `screenshot` or a language note in the format `enPT`). React with ✅ to submit. This will be cancelled in two minutes.")
-                        .addFields(
-                            { name: "String ID", value: result.id },
-                            { name: "Context", value: result.context }
-                        )
+                        .setTitle("Add context for " + args[1])
+                        .setDescription("The context entry has been added! Execute `+context get " + result.id + "` to view the result.")
                         .setFooter("Executed by " + message.author.tag);
-                    if (result.bg) { if (result.bg.length > 1) { embed.addFields({ name: "Note for Bulgarian", value: result.bg, inline: true }) } }
-                    if (result.zhCN) { if (result.zhCN.length > 1) { embed.addFields({ name: "Note for Chinese (Simplified)", value: result.zhCN, inline: true }) } }
-                    if (result.zhTW) { if (result.zhTW.length > 1) { embed.addFields({ name: "Note for Chinese (Traditional)", value: result.zhTW, inline: true }) } }
-                    if (result.cs) { if (result.cs.length > 1) { embed.addFields({ name: "Note for Czech", value: result.cs, inline: true }) } }
-                    if (result.da) { if (result.da.length > 1) { embed.addFields({ name: "Note for Danish", value: result.da, inline: true }) } }
-                    if (result.nl) { if (result.nl.length > 1) { embed.addFields({ name: "Note for Dutch", value: result.nl, inline: true }) } }
-                    if (result.fi) { if (result.fi.length > 1) { embed.addFields({ name: "Note for Finnish", value: result.fi, inline: true }) } }
-                    if (result.fr) { if (result.fr.length > 1) { embed.addFields({ name: "Note for French", value: result.fr, inline: true }) } }
-                    if (result.de) { if (result.de.length > 1) { embed.addFields({ name: "Note for German", value: result.de, inline: true }) } }
-                    if (result.el) { if (result.el.length > 1) { embed.addFields({ name: "Note for Greek", value: result.el, inline: true }) } }
-                    if (result.it) { if (result.it.length > 1) { embed.addFields({ name: "Note for Italian", value: result.it, inline: true }) } }
-                    if (result.ja) { if (result.ja.length > 1) { embed.addFields({ name: "Note for Japanese", value: result.ja, inline: true }) } }
-                    if (result.ko) { if (result.ko.length > 1) { embed.addFields({ name: "Note for Korean", value: result.ko, inline: true }) } }
-                    if (result.no) { if (result.no.length > 1) { embed.addFields({ name: "Note for Norwegian", value: result.no, inline: true }) } }
-                    if (result.enPT) { if (result.enPT.length > 1) { embed.addFields({ name: "Note fer Pirate ☠️", value: result.enPT, inline: true }) } }
-                    if (result.pl) { if (result.pl.length > 1) { embed.addFields({ name: "Note for Polish", value: result.pl, inline: true }) } }
-                    if (result.ptPT) { if (result.ptPT.length > 1) { embed.addFields({ name: "Note for Portuguese", value: result.ptPT, inline: true }) } }
-                    if (result.ptBR) { if (result.ptBR.length > 1) { embed.addFields({ name: "Note for Brazilian", value: result.ptBR, inline: true }) } }
-                    if (result.ru) { if (result.ru.length > 1) { embed.addFields({ name: "Note for Russian", value: result.ru, inline: true }) } }
-                    if (result.esES) { if (result.esES.length > 1) { embed.addFields({ name: "Note for Spanish", value: result.esES, inline: true }) } }
-                    if (result.svSE) { if (result.svSE.length > 1) { embed.addFields({ name: "Note for Swedish", value: result.svSE, inline: true }) } }
-                    if (result.th) { if (result.th.length > 1) { embed.addFields({ name: "Note for Thai", value: result.th, inline: true }) } }
-                    if (result.tr) { if (result.tr.length > 1) { embed.addFields({ name: "Note for Turkish", value: result.tr, inline: true }) } }
-                    if (result.uk) { if (result.uk.length > 1) { embed.addFields({ name: "Note for Ukrainian", value: result.uk, inline: true }) } }
                     msg.edit(embed)
                 }
             })
+            collector.on('end'), collected => {
+                if (!collected) {
+                    const embed = new Discord.MessageEmbed()
+                        .setColor(errorColor)
+                        .setTitle("Add context for " + args[1])
+                        .setDescription("You didn't reply in time, so the adding of the entry has been cancelled.")
+                    msg.edit(embed)
+                }
+            }
         })
     })
 }
