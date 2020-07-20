d3ac5c41b5e737a1dbeaae1f673dad4e0e0cdad1
QkeleQ10 <quintenra@gmail.com>
2020-07-20 16:21:46 +0200

    support for quote links

--- commands/quote.js
+++ commands/quote.js
@@ -4,45 +4,45 @@ const { GoogleSpreadsheet } = require('google-spreadsheet')
 const creds = require('../service-account.json')
 
 module.exports = {
-  name: "quote",
-  description: "Get a funny/weird/wise quote from the server.",
-  usage: "quote",
-  cooldown: 10,
-  allowDM: true,
-  channelBlackList: "621298919535804426",
-  execute(message) {
-    const embed = new Discord.MessageEmbed()
-      .setColor(workingColor)
-      .setTitle("Quote")
-      .setDescription("One second...")
-      .setFooter("Asked for by " + message.author.tag);
-    message.channel.send(embed).then(msg => {
-      accessSpreadsheet(message, msg)
-    })
-  }
+    name: "quote",
+    description: "Get a funny/weird/wise quote from the server.",
+    usage: "quote",
+    cooldown: 10,
+    allowDM: true,
+    channelBlackList: "621298919535804426",
+    execute(message) {
+        const embed = new Discord.MessageEmbed()
+            .setColor(workingColor)
+            .setTitle("Quote")
+            .setDescription("One second...")
+            .setFooter("Asked for by " + message.author.tag);
+        message.channel.send(embed).then(msg => {
+            accessSpreadsheet(message, msg)
+        })
+    }
 };
 
 async function accessSpreadsheet(message, msg) {
-  const doc = new GoogleSpreadsheet('16ZCwOE3Wsfd39-NcEB6QJJZXVyFPEWIWITg0aThcDZ8')
-  await doc.useServiceAccountAuth(creds)
+    const doc = new GoogleSpreadsheet('16ZCwOE3Wsfd39-NcEB6QJJZXVyFPEWIWITg0aThcDZ8')
+    await doc.useServiceAccountAuth(creds)
 
-  await doc.loadInfo()
-  console.log(doc.title)
+    await doc.loadInfo()
+    console.log(doc.title)
 
-  const sheet = doc.sheetsByIndex[0]
-  console.log(sheet.title)
+    const sheet = doc.sheetsByIndex[0]
+    console.log(sheet.title)
 
-  const rows = await sheet.getRows()
+    const rows = await sheet.getRows()
 
-  const rowNum = Math.floor(Math.random() * Math.floor(rows.length))
-  console.log(rowNum)
+    const rowNum = Math.floor(Math.random() * Math.floor(rows.length))
+    console.log(rowNum)
 
-  const correctRow = rows[rowNum]
-  const embed = new Discord.MessageEmbed()
-    .setColor(successColor)
-    .setTitle(correctRow.quote)
-    .setDescription("_      - " + correctRow.user + "_")
-    .setFooter("Asked for by " + message.author.tag);
-  if (correctRow.link) { embed.setTitle("[" + correctRow.quote + "](" + correctRow.link + ")") }
-  msg.edit(embed)
+    const correctRow = rows[rowNum]
+    const embed = new Discord.MessageEmbed()
+        .setColor(successColor)
+        .setTitle(correctRow.quote)
+        .setDescription("_      - " + correctRow.user + "_")
+        .setFooter("Asked for by " + message.author.tag);
+    if (correctRow.link) { embed.setURL(correctRow.link) }
+    msg.edit(embed)
 }
