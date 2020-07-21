f56ac4b49ebfc9c0406f643409e86ba9d93a7e5c
QkeleQ10 <quintenra@gmail.com>
2020-07-21 16:31:40 +0200

    add +context link/view

--- commands/context.js
+++ commands/context.js
@@ -6,7 +6,7 @@ const creds = require('../service-account.json')
 module.exports = {
     name: "context",
     description: "Gets, adds or edits context for the given string ID. `info` shows you information about this command.",
-    usage: "context get|add|edit|help <arguments>`\nFor more info, run `+context help",
+    usage: "context get|add|edit|view|help <arguments>`\nFor more info, run `+context help",
     channelBlackList: ["621298919535804426", "619662798133133312", "712046319375482910", "550951034332381184", "634101000340504576", "713084081579098152"],
     cooldown: 3,
     execute(message, args) {
@@ -30,11 +30,12 @@ module.exports = {
                 else if (args[0] === "get") { getFromSpreadsheet(message, args, msg) }
                 else if (args[0] === "edit") { editInSpreadsheet(message, args, msg) }
                 else if (args[0] === "info" || args[0] === "help") { showInfo(message, args, msg) }
+                else if (args[0] === "view" || args[0] === "link") { viewSheet(message, args, msg) }
                 else {
                     const embed = new Discord.MessageEmbed()
                         .setColor(errorColor)
                         .setTitle("Context")
-                        .setDescription("Please enter a subcommand as first argument. This can either be `get`, `add`, `edit` or `help`.\nFor information, run `+context help`.")
+                        .setDescription("Please enter a subcommand as first argument. This can either be `get`, `add`, `edit`, `link` or `help`.\nFor information, run `+context help`.")
                         .setFooter("Executed by " + message.author.tag);
                     msg.edit(embed)
                 }
@@ -393,9 +394,21 @@ async function showInfo(message, args, msg) {
             { name: "Get", value: "_Gets context for given string_\n`+context get <string ID>`\n\nReplace <string ID> with the ID of the string, found by copying the string URL. It's the number after the #." },
             { name: "Add", value: "_Adds a context entry_\n`+context add <string ID> <context>`\n\nReplace <string ID> with the ID of the string, found by copying the string URL. It's the number after the #. Replace <context> with the text you want to add. After running, you can add more fields using the reactions." },
             { name: "Edit", value: "_Edits an existing context entry_\n`+context edit <string ID> <field> <new value>`\n\nReplace <string ID> with the ID of the string, found by copying the string URL. It's the number after the #. Replace <field> with the field you want to edit, such as `screenshot` or `enPT`. Replace <new value> with the new value for that field." },
+            { name: "View", value: "_Shows the sheet containing context online_\n`+context view`" },
             { name: "Help", value: "_Shows this message!_\n`+context help`" },
             { name: "Fields", value: "id, context, screenshot, bg, zhCN, zhTW, cs, da, nl, fi, fr, de, el, it, ja, ko, no, enPT, pl, ptPT, ptBR, ru, esES, svSE, th, tr, uk" }
         )
         .setFooter("Executed by " + message.author.tag);
     msg.edit(embed)
+    return;
+}
+
+async function viewSheet(message, args, msg) {
+    const embed = new Discord.MessageEmbed()
+        .setColor(successColor)
+        .setTitle("Context sheet")
+        .setDescription("[Click here to open the Google Sheet containing context information.](https://docs.google.com/spreadsheets/d/1tMoGTEZklK-aQhn9r6iRIXuwQXKBkwxZfr4F8LJIwCs)\nOnly specific people can open this sheet. If you'd like to open it as well, please ask <@241926666400563203> or <@240875059953139714> for access.")
+        .setFooter("Executed by " + message.author.tag);
+    msg.edit(embed)
+    return;
 }
