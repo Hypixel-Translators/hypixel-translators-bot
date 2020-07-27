d19db86559f5a3dc011095ad58840e06f7dfe1cb
QkeleQ10 <quintenra@gmail.com>
2020-07-27 14:30:20 +0200

    Update reviewStrings.js

--- events/reviewStrings.js
+++ events/reviewStrings.js
@@ -3,8 +3,9 @@ const Discord = require("discord.js");
 
 module.exports = {
     execute(reaction) {
+        reaction.message.react("⏱")
         setTimeout(() => {
-            if (reaction.message.reactions.cache.get(reaction).count > 0) { reaction.message.delete() }
+            reaction.message.delete() 
         }, 10000)
     }
 }
