dfa997072ada6cfd6fc85b4f1030f41cb5684ecf
Quinten Althues <quintenra@gmail.com>
2020-07-29 21:00:57 +0200

    poll.js arg splitter

--- commands/poll.js
+++ commands/poll.js
@@ -4,8 +4,10 @@ const Discord = require("discord.js");
 module.exports = {
     name: "poll",
     description: "Creates a poll in the current channel. Arguments are split ",
-    usage: "poll <question>-<",
+    usage: "poll <role to ping|'none'>/<question>/<a1 emoji>/<a1 text>/<a2 emoji>/<a2 text>[/...]",
     cooldown: 30,
     execute(message) {
+        const args = message.content.slice(6).split("/");
+        
     }
 }
