7b8be88b41c720641f9d11f1baf015e1d9fb4df8
QkeleQ10 <quintenra@gmail.com>
2020-08-16 23:43:12 +0200

    MISPLACED A BRACKET

--- commands/context.js
+++ commands/context.js
@@ -502,7 +502,7 @@ async function viewSheet(message, args, msg) {
         .setColor(successColor)
         .setTitle("Context sheet")
         .setDescription("[Click here to open the Google Sheet containing context information.](https://docs.google.com/spreadsheets/d/1tVLWskn4InBeopmRdQyrDumr1H6STqyidcEwoL4a8ts)\n\nOnly specific people can open this sheet. You can request access on the page you see when following the link. Please fill in your Crowdin profile link (`.../profile/<username>`) and your Discord username.\nKeep in mind Stannya, Rodry and QkeleQ10 can see your email address.")
-        .setFooter("Executed by " + message.author.tag) + " | Deleting message after 2 minutes";
+        .setFooter("Executed by " + message.author.tag + " | Deleting message after 2 minutes");
     msg.edit(embed)
         .then(linkMsg => {
             setTimeout(() => {
