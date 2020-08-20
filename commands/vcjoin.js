2a5bc5190ead20ad26818353fd7aab5a18ebcd7e
QkeleQ10 <quintenra@gmail.com>
2020-08-20 19:04:13 +0200

    ogg

--- commands/vcjoin.js
+++ commands/vcjoin.js
@@ -1,5 +1,6 @@
 const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
 const Discord = require("discord.js");
+const fs = require('fs');
 
 module.exports = {
   name: "vcjoin",
@@ -18,7 +19,7 @@ module.exports = {
 async function joinVoice(message) {
   if (message.member.voice.channel) {
     const connection = await message.member.voice.channel.join();
-    const dispatcher = connection.play('https://media.vocaroo.com/mp3/cTRMtGxagDC');
+    const dispatcher = connection.play(fs.createReadStream('https://sndup.net/9j79/Greetings-It-is-me-I-can-speak-I-will-leave-now-Salutations.ogg'), { type: 'ogg/opus' });
     dispatcher.on('finish', () => {
       connection.disconnect();
     });
