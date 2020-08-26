dceb584687320e94ccd7645ad565ab8150f05309
QkeleQ10 <quintenra@gmail.com>
2020-08-26 21:28:24 +0200

    add stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,5 +1,6 @@
 const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
 const Discord = require("discord.js");
+const fetch = require("fetch");
 
 module.exports = {
     name: "bug",
@@ -10,6 +11,18 @@ module.exports = {
     allowDM: true,
     channelBlackList: "621298919535804426",
     execute(message, args) {
-
+        fetch(message, args)
     }
+}
+
+async function fetch(message, args) {
+    const embed = new Discord.MessageEmbed()
+        .setColor(successColor)
+        .setTitle("Language status")
+        .setFooter("Executed by " + message.author.tag);
+    let url = "https://api.crowdin.com/api/project/hypixel/language-status?login=qkeleq10&account-key=8205d22af119c4233b1940265bdd77d9&json"
+    result = await fetch.url
+    result.forEach(r => {
+        embed.addFields({ name: r.name, value: (r.translated + "translated _(" + ((100 * r.translated) / r.phrases) + "% from " + r.phrases + ")_, " + r.approved + " approved _(" + ((100 * r.translated) / r.phrases) + "% from " + r.phrases + ")_") })
+    });
 }
