539d1ad8647417771f405a7cc0e48e7a4d047ddb
QkeleQ10 <quintenra@gmail.com>
2020-08-26 21:37:01 +0200

    WHYDOIALWAYSFORGETasyncREEEEEE

--- commands/stats.js
+++ commands/stats.js
@@ -24,7 +24,7 @@ async function fetch(message, args) {
     let url = "https://api.crowdin.com/api/project/hypixel/language-status?login=qkeleq10&account-key=8205d22af119c4233b1940265bdd77d9&json"
     result = await fetch.url
 
-    result.forEach((r, index, array) => {
+    result.forEach(async (r, index, array) => {
         embed.addFields({ name: r.name, value: (r.translated + "translated _(" + ((100 * r.translated) / r.phrases) + "% from " + r.phrases + ")_, " + r.approved + " approved _(" + ((100 * r.translated) / r.phrases) + "% from " + r.phrases + ")_") })
         await itemsProcessed++
         if (itemsProcessed === array.length) {
