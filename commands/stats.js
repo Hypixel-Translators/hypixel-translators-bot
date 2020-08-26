770f71c8eddee6e082919f6526747ba0a23ce79f
QkeleQ10 <quintenra@gmail.com>
2020-08-26 22:23:58 +0200

    idk how to not get jquery slim wtf

--- commands/stats.js
+++ commands/stats.js
@@ -1,7 +1,7 @@
 const { workingColor, errorColor, successColor, neutralColor } = require("../config.json");
 const Discord = require("discord.js");
 const fetch = require("fetch");
-const $ = require("jquery")
+const $ = require("../jquery-3.5.0.js")
 
 module.exports = {
     name: "stats",
@@ -22,8 +22,8 @@ async function get(message, args) {
         .setTitle("Language status")
         .setFooter("Executed by " + message.author.tag);
 
-    $.getJSON("https://api.crowdin.com/api/project/hypixel/language-status?login=qkeleq10&account-key=8205d22af119c4233b1940265bdd77d9&json", function (out) {
-        out.forEach(async (r, index, array) => {
+    jQuery.getJSON("https://api.crowdin.com/api/project/hypixel/language-status?login=qkeleq10&account-key=8205d22af119c4233b1940265bdd77d9&json", function (data) {
+        data.forEach(async (r, index, array) => {
             embed.addFields({ name: r.name, value: (r.translated + "translated _(" + ((100 * r.translated) / r.phrases) + "% from " + r.phrases + ")_, " + r.approved + " approved _(" + ((100 * r.translated) / r.phrases) + "% from " + r.phrases + ")_") })
             await itemsProcessed++
             if (itemsProcessed === array.length) {
