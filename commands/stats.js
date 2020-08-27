88530afaf5ec9a27def44d804292ea7a906480cd
QkeleQ10 <quintenra@gmail.com>
2020-08-27 19:03:50 +0200

    fixi

--- commands/stats.js
+++ commands/stats.js
@@ -17,7 +17,6 @@ async function get(message, args) {
     let url = "https://api.crowdin.com/api/project/hypixel/status?login=qkeleq10&account-key=8205d22af119c4233b1940265bdd77d9&json"
     let settings = { method: "Get" }
     var index = 0
-
     fetch(url, settings)
         .then(res => res.json())
         .then((json) => {
@@ -30,14 +29,13 @@ async function get(message, args) {
                         const embed = new Discord.MessageEmbed()
                             .setColor(successColor)
                             .setTitle(langdbEntry.emoji + " | " + r.name)
-                            .addFields({ name: (Math.round((100 * r.translated) / r.phrases) + "% translated (" + r.translated + "/" + r.phrases + ")"), value: (Math.round((100 * r.approved) / r.phrases) + "% approved (" + r.approved + "/" + r.phrases + ")\n\n_Translate on https://crowdin.com/project/hypixel/" + r.code) })
+                            .addFields({ name: (Math.round((100 * r.translated) / r.phrases) + "% translated (" + r.translated + "/" + r.phrases + ")"), value: (Math.round((100 * r.approved) / r.phrases) + "% approved (" + r.approved + "/" + r.phrases + ")\n\n_Translate on https://crowdin.com/project/hypixel/" + r.code + "_") })
                             //.addFields({ name: r.name, value: ("**" + r.translated + " translated** (" + Math.round((100 * r.translated) / r.phrases) + "% from " + r.phrases + ")\n**" + r.approved + " approved** (" + Math.round((100 * r.approved) / r.phrases) + "% from " + r.phrases + ")"), inline: true })
                             .setTimestamp()
                         msg.edit("", embed)
                         index++
                     })
                 })
-
             message.client.channels.cache.get("730042612647723058").messages.fetch("748584877921796146")
                 .then(stringCount => {
                     if (stringCount.content !== json[0].phrases) {
