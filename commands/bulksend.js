340922f43e25759536498cc384c817115a1c5beb
QkeleQ10 <quintenra@gmail.com>
2020-08-27 22:39:16 +0200

    fuk

--- commands/bulksend.js
+++ commands/bulksend.js
@@ -6,22 +6,8 @@ module.exports = {
   allowDM: true,
   execute(message, args) {
     if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    const embed = new Discord.MessageEmbed()
-      .setColor(workingColor)
-      .setTitle("Bulk send")
-      .setDescription("Sending...")
-      .setFooter("Executed by " + message.author.tag);
-    message.channel.send(embed)
-      .then(msg => {
-        const sendTo = msg.client.channels.cache.get(args[0])
-        sendmsg(sendTo, args[1])
-        const embed = new Discord.MessageEmbed()
-          .setColor(successColor)
-          .setTitle("Bulk send")
-          .setDescription("Sent!")
-          .setFooter("Executed by " + message.author.tag);
-        msg.edit(embed)
-      })
+    const sendTo = msg.client.channels.cache.get(args[0])
+    sendmsg(sendTo, args[1])
   }
 };
 
