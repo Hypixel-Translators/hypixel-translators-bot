073bd8f7486f4102f8252a6c60dd6abd1972da17
QkeleQ10 <quintenra@gmail.com>
2020-08-29 11:13:35 +0200

    less intensive check on main file for smaller checks on other files

--- commands/language.js
+++ commands/language.js
@@ -40,19 +40,12 @@ module.exports = {
                             });
                         }
 
-                        fs.exists('/strings/' + args[0] + "", function (err, result) {
-                            if (result) {
-                                message.client.channels.cache.get("748968125663543407").send(message.author.id + " " + args[0])
-                                strings = require(("../strings/" + args[0] + "/language.json"))
-                                var currentTime = new Date().getTime(); while (currentTime + 100 >= new Date().getTime()) { };
-                                const embed = new Discord.MessageEmbed()
-                                    .setColor(successColor)
-                                    .setAuthor(strings.moduleName)
-                                    .setTitle(strings.changedToTitle)
-                                    .setFooter(strings.executedBy + message.author.tag);
-                                msg.edit(embed)
-                            }
+
+                        const path = '../strings/' + args[0] + '/language.json'
+
+                        fs.access(path, fs.F_OK, (err) => {
                             if (err) {
+                                console.error(err)
                                 const embed = new Discord.MessageEmbed()
                                     .setColor(errorColor)
                                     .setAuthor(strings.moduleName)
@@ -62,6 +55,15 @@ module.exports = {
                                 msg.edit(embed)
                                 return
                             }
+                            message.client.channels.cache.get("748968125663543407").send(message.author.id + " " + args[0])
+                            strings = require(("../strings/" + args[0] + "/language.json"))
+                            var currentTime = new Date().getTime(); while (currentTime + 100 >= new Date().getTime()) { };
+                            const embed = new Discord.MessageEmbed()
+                                .setColor(successColor)
+                                .setAuthor(strings.moduleName)
+                                .setTitle(strings.changedToTitle)
+                                .setFooter(strings.executedBy + message.author.tag);
+                            msg.edit(embed)
                         })
                     })
             })
