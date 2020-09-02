adcc8ffc1265f9239e5ff9075df32b229135e272
QkeleQ10 <quintenra@gmail.com>
2020-09-02 18:14:12 +0200

    fix all kinds of things and add new strings, and allow codes to be entered in +mention

--- commands/mention.js
+++ commands/mention.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
+const { langdb } = require("../config.json")
 
 module.exports = {
   name: "mention",
   description: "Mentions a language role with a message.",
   aliases: ["langping", "languageping"],
   usage: "mention <language> <proofreader|translator|all> [message]",
-  cooldown: 320,
+  cooldown: 120,
   categoryBlackList: ["549503328472530975"],
   execute(strings, message, args) {
     const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
-    console.log(args)
     var type = args[1]
     const lowerArg = args[0].toLowerCase()
     var toLook = lowerArg.charAt(0).toUpperCase() + lowerArg.slice(1)
@@ -19,75 +19,68 @@ module.exports = {
       toSend = "<a:bongoping:614477510423478275>" + toSend
     }
 
-    if (toLook === "Chinesesimplified" || toLook === "Chinese-simplified") {
-      toLook = "Chinese (Simplified)"
-    }
-    if (toLook === "Chinesetraditional" || toLook === "Chinese-traditional") {
-      toLook = "Chinese (Traditional)"
-    }
-    if (toLook === "Lolcat") {
-      toLook = "LOLCAT"
-    }
+    if (toLook === "Chinesesimplified" || toLook === "Chinese-simplified" || toLook === "Zhcn") { toLook = "Chinese (Simplified)" }
+    if (toLook === "Chinesetraditional" || toLook === "Chinese-traditional" || toLook === "Zhtw") { toLook = "Chinese (Traditional)" }
+    if (toLook === "Lolcat" || toLook === "Lol") { toLook = "LOLCAT" }
+    if (toLook === "Bg") { toLook = "Bulgarian" }
+    if (toLook === "Cs") { toLook = "Czech" }
+    if (toLook === "Da") { toLook = "Danish" }
+    if (toLook === "Nl") { toLook = "Dutch" }
+    if (toLook === "Fi") { toLook = "Finnish" }
+    if (toLook === "Fr") { toLook = "French" }
+    if (toLook === "De") { toLook = "German" }
+    if (toLook === "El") { toLook = "Greek" }
+    if (toLook === "It") { toLook = "Italian" }
+    if (toLook === "Ja") { toLook = "Japanese" }
+    if (toLook === "Ko") { toLook = "Korean" }
+    if (toLook === "No") { toLook = "Norwegian" }
+    if (toLook === "Pl") { toLook = "Polish" }
+    if (toLook === "Pt") { toLook = "Portuguese" }
+    if (toLook === "Ptbr") { toLook = "Brazilian" }
+    if (toLook === "Ru") { toLook = "Russian" }
+    if (toLook === "Es") { toLook = "Spanish" }
+    if (toLook === "Sv") { toLook = "Swedish" }
+    if (toLook === "Th") { toLook = "Thai" }
+    if (toLook === "Tr") { toLook = "Turkish" }
+    if (toLook === "Ua") { toLook = "Ukrainian" }
+    if (toLook === "Enpt") { toLook = "Pirate English" }
 
-    console.log(toLook)
-    console.log(toSend)
-    console.log(type)
+
+    console.log("toLook: " + toLook)
+    console.log("toSend: " + toSend)
+    console.log("type: " + type)
     const role = message.guild.roles.cache.find(x => x.name == (toLook + " Proofreader"))
 
     if (!role) {
-      message.channel.send("The role you entered doesn't exist. Make sure not to use abbreviations. For Chinese (Simplified/Traditional), use \`Chinese-simplified/traditional\`.");
+      message.channel.send(strings.errorNotFound + "`bg`, `cs`, `da`, `de`, `el`, `enpt`, `es`, `fi`, `fr`, `it`, `ja`, `ko`, `lol`, `nl`, `no`, `pl`, `pt`, `ptbr`, `ru`, `sv`, `th`, `tr`, `ua`, `zhcn`, `zhtw`.");
       return;
     }
     if (type === "pf" || type === "pr" || type === "proofreader" || type === "Proofreader") {
       const toPing = message.guild.roles.cache.find(role => role.name === toLook + " Proofreader");
       const lowerRole = message.guild.roles.cache.find(role => role.name === toLook + " Translator");
       console.log(toPing + "\n" + lowerRole);
-      if (
-        message.member.roles.cache.find(
-          role => role.name === toLook + " Proofreader" || message.member.hasPermission("ADMINISTRATOR")
-        )
-      ) {
+      if (message.member.roles.cache.find(role => role.name === toLook + " Proofreader" || message.member.hasPermission("ADMINISTRATOR"))) {
         message.delete()
         message.channel.send("**<@" + message.member.id + ">**: <@&" + toPing + "> " + toSend);
-      } else {
-        message.channel.send(
-          "You don't have that language's proofreader role, so you can't mention it. Contact the server owner or administrator if you really need to."
-        );
-      }
+      } else { message.channel.send(strings.errorNoPing + strings.errorNoPingPr + strings.errorNoPingDisclaimer) }
     } else if (type === "tr" || type === "translator" || type === "Translator") {
       const toPing = message.guild.roles.cache.find(role => role.name === toLook + " Translator");
       const higherRole = message.guild.roles.cache.find(role => role.name === toLook + " Proofreader");
       console.log(toPing + "\n" + higherRole);
-      if (
-        message.member.roles.cache.find(
-          role => role.name === toLook + " Proofreader" || message.member.hasPermission("ADMINISTRATOR")
-        )
+      if (message.member.roles.cache.find(role => role.name === toLook + " Proofreader" || message.member.hasPermission("ADMINISTRATOR"))
       ) {
         message.delete()
         message.channel.send("**<@" + message.member.id + ">**: <@&" + toPing + "> " + toSend);
-      } else {
-        message.channel.send(
-          "You don't have that language's proofreader role, so you can't mention its translator role. Contact the server owner or administrator if you really need to."
-        );
-      }
+      } else { message.channel.send(strings.errorNoPing + strings.errorNoPingTr + strings.errorNoPingDisclaimer) }
     } else if (type === "all" || type === "both") {
       const translatorPing = message.guild.roles.cache.find(role => role.name === toLook + " Translator");
       const proofreaderPing = message.guild.roles.cache.find(role => role.name === toLook + " Proofreader");
       console.log(translatorPing + "\n" + proofreaderPing);
-      if (
-        message.member.roles.cache.find(
-          role => role.name === toLook + " Proofreader" || message.member.hasPermission("ADMINISTRATOR")
-        )
-      ) {
+      if (message.member.roles.cache.find(role => role.name === toLook + " Proofreader" || message.member.hasPermission("ADMINISTRATOR"))) {
         message.delete()
         message.channel.send("**<@" + message.member.id + ">**: <@&" + translatorPing + "> <@&" + proofreaderPing + "> " + toSend);
-      } else {
-        message.channel.send(
-          "You don't have that language's proofreader role, so you can't mention the entire language. Contact the server owner or administrator if you really need to."
-        );
-      }
+      } else { message.channel.send(strings.errorNoPing + strings.errorNoPingAll + strings.errorNoPingDisclaimer) }
     }
-
   }
 };
 
