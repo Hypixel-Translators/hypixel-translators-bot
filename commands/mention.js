8a8699862703816ac1304430467d71de738c0b53
QkeleQ10 <quintenra@gmail.com>
2020-09-04 22:37:58 +0200

    update blacklisto

--- commands/mention.js
+++ commands/mention.js
@@ -7,7 +7,7 @@ module.exports = {
   aliases: ["langping", "languageping"],
   usage: "mention <language> <proofreader|translator|all> [message]",
   cooldown: 120,
-  categoryBlackList: ["549503328472530975"],
+  channelBlackList: ["621298919535804426", "619662798133133312", "712046319375482910", "644620638878695424", "550951034332381184", "549894938712866816", "713084081579098152"],
   execute(strings, message, args) {
     const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
     var type = args[1]
