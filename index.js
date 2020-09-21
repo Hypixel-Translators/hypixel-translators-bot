c09d984e60a55be524e65b974335d8b11dc922e3
QkeleQ10 <quintenra@gmail.com>
2020-09-21 22:59:18 +0200

    SATISFIED NOW?

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ client.on("message", async message => {
     if (!user.roles.cache.has("569194996964786178")) return;
   }
 
-  var oldMessages = message.client.channels.cache.get("748968125663543407").messages.fetch() //languages database
-  var oldFiMessages = oldMessages.filter(element => element.content.includes(message.author.id))
+  const oldMessages = await message.client.channels.cache.get("748968125663543407").messages.fetch() //languages database
+  const oldFiMessages = await oldMessages.filter(element => element.content.includes(message.author.id))
   oldFiMessages.forEach(async element => {
     oldMsg = element.content.split(" ")
     oldMsg.splice(oldMsg.indexOf(message.author.id), 1)
