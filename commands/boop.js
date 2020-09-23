9f4268fb58ba795a5caba10705b668f9ab37ee03
QkeleQ10 <quintenra@gmail.com>
2020-09-23 14:28:01 +0200

    am idot

--- commands/boop.js
+++ commands/boop.js
@@ -12,8 +12,8 @@ module.exports = {
         var userToSend = args[0].replace(/[\\<>@#&!]/g, "")
         var boop = "<:Fr:758299142765805599><:om:758299142865944627> " + message.author.tag + "<:B_:758300178784059402><:oo:758299143059275816><:p_:758299143021395978>"
         try {
-            if (message.client.members.cache.get(userToSend).presence.status === "online") {
-                message.client.members.cache.get(userToSend).send(boop)
+            if (message.client.users.cache.get(userToSend).presence.status === "online") {
+                message.client.users.cache.get(userToSend).send(boop)
             }
         } catch (error) { console.error(error); throw "falseUser"; }
     }
