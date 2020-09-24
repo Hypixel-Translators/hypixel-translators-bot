430b8b3113c5c465f152ce86d9605f06cf3ec579
ImRodry <38259440+ImRodry@users.noreply.github.com>
2020-09-24 23:08:24 +0100

    Whitelist bot translators

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports = {
   aliases: ["invitelink"],
   usage: "invite",
   cooldown: 120,
-  channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058"],
+  channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335"],
   execute(strings, message) {
     const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
     //message.delete();
