da23ef06bbd9d740651299bf7a642a51ef4ca331
QkeleQ10 <quintenra@gmail.com>
2020-09-25 10:14:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ client.on("message", async message => {
     }
   }
 
-  if (message.member) if (message.member.hasPermission("BAN_MEMBERS")) timestamps.set(message.author.id, now);
+  if (message.member) if (message.member.hasPermission("KICK_MEMBERS")) timestamps.set(message.author.id, now);
   setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
   var strings = require(("./strings/en/" + command.name + ".json"))
