c7d17d3d1897250005d1f89f213494a1dc76cd10
Quinten Althues <quintenra@gmail.com>
2020-09-27 14:50:11 +0200

    log lines

--- events/stats.js
+++ events/stats.js
@@ -133,49 +133,53 @@ async function bot(client) {
 }
 
 async function skyblockaddons(client) {
-    let url = "https://api.crowdin.com/api/project/skyblockaddons/status?login=qkeleq10&account-key=" + ctoken + "&json"
-    let settings = { method: "Get" }
-    var index = 0
-    fetch(url, settings)
-        .then(res => res.json())
-        .then((json) => {
-            json.reverse()
-            client.channels.cache.get("748626115530326016").messages.fetch()
-                .then(messages => {
-                    fiMessages = messages.filter(msg => msg.author.bot)
-                    fiMessages.forEach(async (msg) => {
-                        var r = json[index]
-                        var langdbEntry = langdb.find(o => o.name === r.name)
+    try {
+        let url = "https://api.crowdin.com/api/project/skyblockaddons/status?login=qkeleq10&account-key=" + ctoken + "&json"
+        let settings = { method: "Get" }
+        var index = 0
+        fetch(url, settings)
+            .then(res => res.json())
+            .then((json) => {
+                json.reverse()
+                client.channels.cache.get("748626115530326016").messages.fetch()
+                    .then(messages => {
+                        fiMessages = messages.filter(msg => msg.author.bot)
+                        fiMessages.forEach(async (msg) => {
+                            var r = json[index]
+                            console.log(r.name)
+                            var langdbEntry = langdb.find(o => o.name === r.name)
+                            console.log(langdbEntry.name)
 
-                        if (r.approved_progress > 89) {
-                            adapColour = successColor
-                        } else if (r.approved_progress > 49) {
-                            adapColour = workingColor
-                        } else {
-                            adapColour = errorColor
-                        }
+                            if (r.approved_progress > 89) {
+                                adapColour = successColor
+                            } else if (r.approved_progress > 49) {
+                                adapColour = workingColor
+                            } else {
+                                adapColour = errorColor
+                            }
 
-                        const embed = new Discord.MessageEmbed()
-                            .setColor(adapColour)
-                            .setTitle(langdbEntry.emoji || "<:icon_question:756582065834688662>" + " | " + r.name)
-                            .setDescription("**" + r.translated_progress + "% translated (" + r.translated + "/" + r.phrases + " strings)**\n" + r.approved_progress + "% approved (" + r.approved + "/" + r.phrases + " strings)\n\nTranslate at https://crowdin.com/project/quickplay/" + r.code)
-                            //.addFields({ name: (r.translated_progress + "% translated (" + r.translated + "/" + r.phrases + " strings)"), value: (r.approved_progress + "% approved (" + r.approved + "/" + r.phrases + " strings)\n\nTranslate at https://crowdin.com/project/quickplay/" + r.code + "") })
-                            .setTimestamp()
-                        msg.edit("", embed)
-                        index++
+                            const embed = new Discord.MessageEmbed()
+                                .setColor(adapColour)
+                                .setTitle(langdbEntry.emoji || "<:icon_question:756582065834688662>" + " | " + r.name)
+                                .setDescription("**" + r.translated_progress + "% translated (" + r.translated + "/" + r.phrases + " strings)**\n" + r.approved_progress + "% approved (" + r.approved + "/" + r.phrases + " strings)\n\nTranslate at https://crowdin.com/project/quickplay/" + r.code)
+                                //.addFields({ name: (r.translated_progress + "% translated (" + r.translated + "/" + r.phrases + " strings)"), value: (r.approved_progress + "% approved (" + r.approved + "/" + r.phrases + " strings)\n\nTranslate at https://crowdin.com/project/quickplay/" + r.code + "") })
+                                .setTimestamp()
+                            msg.edit("", embed)
+                            index++
+                        })
                     })
-                })
 
-            client.channels.cache.get("730042612647723058").messages.fetch("758819913577136190")
-                .then(stringCount => {
-                    if (stringCount.content !== json[0].phrases) {
-                        if (stringCount.content < json[0].phrases) {
-                            client.channels.cache.get("748594964476329994").send("> <a:coolparty:728990234930315344> **New String(s)!**\n" + Number(Number(json[0].phrases) - Number(stringCount.content)) + " string(s) have been added to the SkyblockAddons project.\n\nTranslate at <https://crowdin.com/project/skyblockaddons>")
-                        } else {
-                            client.channels.cache.get("748594964476329994").send("> <:vote_no:732298639736570007> **String(s) Removed**\n" + Math.abs(Number(Number(json[0].phrases) - Number(stringCount.content))) + " string(s) have been removed from the SkyblockAddons project.")
+                client.channels.cache.get("730042612647723058").messages.fetch("758819913577136190")
+                    .then(stringCount => {
+                        if (stringCount.content !== json[0].phrases) {
+                            if (stringCount.content < json[0].phrases) {
+                                client.channels.cache.get("748594964476329994").send("> <a:coolparty:728990234930315344> **New String(s)!**\n" + Number(Number(json[0].phrases) - Number(stringCount.content)) + " string(s) have been added to the SkyblockAddons project.\n\nTranslate at <https://crowdin.com/project/skyblockaddons>")
+                            } else {
+                                client.channels.cache.get("748594964476329994").send("> <:vote_no:732298639736570007> **String(s) Removed**\n" + Math.abs(Number(Number(json[0].phrases) - Number(stringCount.content))) + " string(s) have been removed from the SkyblockAddons project.")
+                            }
+                            stringCount.edit(json[0].phrases)
                         }
-                        stringCount.edit(json[0].phrases)
-                    }
-                })
-        })
+                    })
+            })
+    } catch (error) { throw error }
 }
