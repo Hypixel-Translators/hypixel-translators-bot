184d34b81fe6385ad0b1f631f17fb26f448a73c1
QkeleQ10 <quintenra@gmail.com>
2020-09-28 22:20:59 +0200

    Revamp +parrot command to give Administrators special rights

--- commands/parrot.js
+++ commands/parrot.js
@@ -5,7 +5,7 @@ module.exports = {
   name: "parrot",
   description: "Say something in a specific channel.",
   usage: "parrot <message>",
-  aliases: ["repeat", "send"],
+  aliases: ["say", "repeat", "send"],
   allowDM: true,
   execute(strings, message, args) {
     const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
@@ -13,40 +13,24 @@ module.exports = {
     args.splice(0, 1)
     var toSend = args.join(" ")
 
-    //message.delete();
     var allowed = false
     if (message.author.id == "722738307477536778") { allowed = true }
     if (message.channel.type !== "dm") { if (message.member.roles.cache.has("621071221462663169") || message.member.roles.cache.has("549885657749913621") || message.member.roles.cache.has("241926666400563203")) { allowed = true } }
-    if (!allowed) return;
+    if (!allowed) throw "noAccess";
+    if (!toSend) throw "noMessage";
 
+    const sendTo = message.client.channels.cache.get(rawSendTo.replace("<#", "").replace(">", ""))
+    if (message.member) if (message.member.hasPermission("ADMINISTRATOR")) {
+      sendTo.send(toSend).catch(err => { throw "noChannel"; })
+    } else {
+      sendTo.send(">>> " + toSend).catch(err => { throw "noChannel"; })
+    }
     const embed = new Discord.MessageEmbed()
-      .setColor(workingColor)
+      .setColor(successColor)
       .setAuthor(strings.moduleName)
-      .setTitle(strings.loading)
+      .setTitle(strings.success)
+      .setDescription("<#" + sendTo.id + ">:\n>>> " + toSend)
       .setFooter(executedBy)
     message.channel.send(embed)
-      .then(msg => {
-        const sendTo = msg.client.channels.cache.get(rawSendTo.replace("<#", "").replace(">", ""))
-        sendTo.send(">>> " + toSend)
-          .catch(err => {
-            msg.delete()
-            throw "noChannel";
-            /*const embed = new Discord.MessageEmbed()
-              .setColor(errorColor)
-              .setAuthor(strings.moduleName)
-              .setTitle(strings.invalidArg)
-              .setFooter(executedBy)
-            if (!rawSendTo) { embed.setDescription(strings.notProvided.replace("%%type%%", strings.aChannel)) }
-            else { embed.setDescription(strings.notFound.replace("%%type%%", strings.thatChannel)) }
-            msg.edit(embed)*/
-          })
-        const embed = new Discord.MessageEmbed()
-          .setColor(successColor)
-          .setAuthor(strings.moduleName)
-          .setTitle(strings.success)
-          .setDescription("<#" + sendTo.id + ">:\n>>> " + toSend)
-          .setFooter(executedBy)
-        msg.edit(embed)
-      })
   }
 };
