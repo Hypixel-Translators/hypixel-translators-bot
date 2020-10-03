db470c36942bd0734ffc392bc05196020085e24f
QkeleQ10 <quintenra@gmail.com>
2020-10-03 15:34:19 +0200

    Update +bug to redirect to issues

--- commands/bug.js
+++ commands/bug.js
@@ -3,44 +3,21 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "bug",
-    description: "Report a bug present in the bot.",
-    usage: "bug <message>",
-    aliases: ["bugreport", "reportbug"],
+    description: "Opens the GitHub issues page.",
+    usage: "bug",
+    aliases: ["issue", "issues"],
     cooldown: 320,
     allowDM: true,
     channelBlackList: "621298919535804426",
-    execute(strings, message, args) {
+    execute(strings, message) {
         const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
-        var toSend = args.join(" ")
-
-        if (!args[0]) {
-            throw "noMessage";
-        }
-
         const embed = new Discord.MessageEmbed()
-            .setColor(workingColor)
+            .setColor(successColor)
             .setAuthor(strings.moduleName)
-            .setTitle(strings.sendingTitle)
-            .setDescription(toSend)
+            .setTitle(strings.bugT)
+            .setDescription(bugD.replace("%%link%%", "(https://github.com/QkeleQ10/hypixel-translators-bot-discord/issues)"))
+            .addFields({ name: strings.urgentT, value: strings.urgentD.replace("%%q10%%", "<@722738307477536778>") })
             .setFooter(executedBy);
         message.channel.send(embed)
-            .then(msg => {
-                const sendTo = msg.client.channels.cache.get("730042612647723058")
-                const report = new Discord.MessageEmbed()
-                    .setColor(neutralColor)
-                    .setAuthor("Bug report")
-                    .setTitle("A fresh bug report has arrived. Time to fix stuff!")
-                    .setDescription(toSend)
-                    .addFields({ name: "To reply", value: "\`+dm " + message.author.id + " \`" })
-                    .setFooter("Reported by " + message.author.tag);
-                sendTo.send(report)
-                const embed = new Discord.MessageEmbed()
-                    .setColor(successColor)
-                    .setAuthor(strings.moduleName)
-                    .setTitle(strings.sentTitle)
-                    .setDescription(toSend)
-                    .setFooter(executedBy);
-                msg.edit(embed)
-            })
     }
 };
