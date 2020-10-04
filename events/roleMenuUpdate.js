a30e7c623abb172bccd22395b30a9047961a12eb
Quinten Althues <quintenra@gmail.com>
2020-10-04 16:57:06 +0200

    idk

--- events/roleMenuUpdate.js
+++ events/roleMenuUpdate.js
@@ -9,7 +9,6 @@ module.exports = {
                 .setTitle("Channels")
                 .setDescription("Some information will go here.")
                 .addFields({ name: "Some header", value: "Some text" })
-                .setFooter("Need help? Ask your questions in #off-topic | Bot made with lots of care by QkeleQ10#6163")
             m1.edit("", embed1)
         })
         client.channels.cache.get("730042612647723058").messages.fetch("762321583317843988").then(m2 => {
@@ -18,7 +17,6 @@ module.exports = {
                 .setTitle("Roles")
                 .setDescription("Some information will go here.")
                 .addFields({ name: "Some header", value: "Some text" })
-                .setFooter("Need help? Ask your questions in #off-topic | Bot made with lots of care by QkeleQ10#6163")
             m2.edit("", embed2)
         })
         client.channels.cache.get("730042612647723058").messages.fetch("762321588942274560").then(m3 => {
