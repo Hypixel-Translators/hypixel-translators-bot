66d6055c434e0c08fdb7655614edbff78aca9a30
QkeleQ10 <quintenra@gmail.com>
2020-10-04 18:28:51 +0200

    UGH now I used the non-existant thing i just undefined that doesn't exist what

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ const cooldowns = new Discord.Collection();
 client.once("ready", () => {
   console.log("Ready!")
 
-  roleMenuUpdate.execute(client)
-
   client.channels.cache.get("732587569744838777").messages.fetch("733036798736990309")
   client.channels.cache.get("732326676192690236").messages.fetch()
   client.channels.cache.get("734081393499308053").messages.fetch()
