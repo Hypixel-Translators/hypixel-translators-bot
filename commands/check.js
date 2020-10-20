73afe79d5321eca14becdff53ad51f43b8ad20f0
Quinten Althues <quintenra@gmail.com>
2020-10-20 12:27:56 +0200

    test user find system

--- commands/check.js
+++ commands/check.js
@@ -16,22 +16,35 @@ module.exports = {
         .setTitle(strings.loading)
         .setFooter(executedBy)
       message.channel.send(embed).then(msg => {
-        var authorP = [] //author perm array
-        var botP = [] //bot perm array
-        var userP = [] //user perm array
-        const authorPerms = message.member.permissions.toArray().forEach(e => { authorP.push(strings.perms[e] || e) })
-        const botPerms = msg.member.permissions.toArray().forEach(e => { botP.push(strings.perms[e] || e) })
-        if (args[0]) { const userPerms = msg.member.permissions.toArray().forEach(e => { userP.push(strings.perms[e] || e) }) }
-        const embed = new Discord.MessageEmbed()
-          .setColor(successColor)
-          .setAuthor(strings.moduleName)
-          .setTitle(strings.list)
-          .addFields(
-            { name: msg.author.tag, value: botP.join(", ") },
-            { name: message.author.tag, value: authorP.join(", ") }
-          )
-          .setFooter(executedBy)
-        msg.edit(embed)
+        if (!args[0]) { //no arguments given - display bot and author perms
+          var authorP = []
+          var botP = []
+          message.member.permissions.toArray().forEach(e => { authorP.push(strings.perms[e] || e) })
+          msg.member.permissions.toArray().forEach(e => { botP.push(strings.perms[e] || e) })
+          const embed = new Discord.MessageEmbed()
+            .setColor(successColor)
+            .setAuthor(strings.moduleName)
+            .setTitle(strings.list)
+            .addFields(
+              { name: msg.author.tag, value: botP.join(", ") },
+              { name: message.author.tag, value: authorP.join(", ") }
+            )
+            .setFooter(executedBy)
+          msg.edit(embed)
+        } else {
+          var user = msg.guild.members.find(m => m.id === args[0].replace("<@", "").replace(">", "")) || m.user.username === args[0] || m.nickname === args[0] || m.user.username.toLowerCase().includes(args[0].toLowerCase()) || m.nickname.toLowerCase().includes(args[0].toLowerCase())
+          var userP = []
+          user.permissions.toArray().forEach(e => { userP.push(strings.perms[e] || e) })
+          const embed = new Discord.MessageEmbed()
+            .setColor(successColor)
+            .setAuthor(strings.moduleName)
+            .setTitle(strings.list)
+            .addFields(
+              { name: strings.uperms.replace("%%uperms%%", user.tag), value: userP.join(", ") }
+            )
+            .setFooter(executedBy)
+          msg.edit(embed)
+        }
       })
     } catch (err) { throw err }
   }
