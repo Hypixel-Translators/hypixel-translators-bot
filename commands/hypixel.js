946f06e6b37417e3c85391d2a4e87fdcceb2fc06
Rodrigo <rodrigoleitao25@gmail.com>
2020-10-25 15:00:32 +0000

    Remove emoji

--- commands/hypixel.js
+++ commands/hypixel.js
@@ -9,6 +9,6 @@ module.exports = {
   channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
   execute(strings, message) {
     //message.delete();
-    message.channel.send(strings.hypixelCrowdin.replace("%%crowdin%%", "<https://crowdin.com/project/hypixel>") + " <:hypixel:614477493771960337>\n" + strings.hypixelGuide + "\n" + strings.hypixelDiscord.replace("%%discord%%", "<https://discord.gg/hypixel>"))
+    message.channel.send(strings.hypixelCrowdin.replace("%%crowdin%%", "<https://crowdin.com/project/hypixel>") + "\n" + strings.hypixelGuide + "\n" + strings.hypixelDiscord.replace("%%discord%%", "<https://discord.gg/hypixel>"))
   }
 };
