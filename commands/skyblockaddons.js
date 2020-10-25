b43b68939cda21b7e76c7cff6bc103eda7c9ce7c
ImRodry <38259440+ImRodry@users.noreply.github.com>
2020-10-25 16:05:47 +0000

    Arrays arrays and more arrays

--- commands/skyblockaddons.js
+++ commands/skyblockaddons.js
@@ -5,7 +5,7 @@ module.exports = {
   name: "skyblockaddons",
   description: "Gives you useful information regarding the SkyblockAddons Crowdin project.",
   usage: "skyblockaddons",
-  aliases: "sba",
+  aliases: ["sba"],
   cooldown: 120,
   channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
   execute(strings, message) {
