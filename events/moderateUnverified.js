01a8d5a6e3bd264bfbbad9909845200e469f68af
Rodrigo <rodrigoleitao25@gmail.com>
2020-11-14 22:30:16 +0000

    try with new role and manual

--- events/moderateUnverified.js
+++ events/moderateUnverified.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 
 module.exports = {
-    execute(client) {
+    execute(client, manual) {
         var d = new Date()
         var h = d.getUTCHours()
         var m = d.getUTCMinutes()
-        if (h == "20" && m == "21") {
+        if (h == "22" && m == "30" || manual) {
             check(client)
         }
     }
@@ -19,10 +19,11 @@ async function check(client) {
     var kick = new Date().getTime() - (14 * 24 * 60 * 60 * 1000)
     //Get date 14 days ago                   d    h    m    s    ms
 
-    await client.guilds.cache.get("549503328472530974").roles.get("739111904672481280").members.forEach(async m => { //guid ID and Unverified
-        if (m.joinedAt > alert) {
-            m.send("Hey there!\nWe noticed you haven't verified yourself on our server. Are you having any trouble? Please message Rodry or Stannya or just ask any questions in the verify channel! Otherwise, please send your profile link like shown in the channel.\n\nThis message was sent to you because you have been on our server for too long, and you're in risk of getting kicked for inactivity soon.\nPlease do not reply to this bot.")
-            client.channels.cache.get("662660931838410754").send("Sent an alert to **<@" + m.id + ">** as they've been stood in the server for 7 days without verifying.") //verify-logs
+    await client.guilds.cache.get("549503328472530974").roles.get("777270531857711114").members.forEach(async member => { //guid ID and Unverified(testing for now)
+        if (member.joinedAt >= alert) {
+            member.send("Hey there!\nWe noticed you haven't verified yourself on our server. Are you having any trouble? Please message Rodry or Stannya or just ask any questions in the verify channel! Otherwise, please send your profile link like shown in the channel.\n\nThis message was sent to you because you have been on our server for too long, and you're in risk of getting kicked for inactivity soon.\nPlease do not reply to this bot.")
+            client.channels.cache.get("662660931838410754").send("Sent an alert to **<@" + member.id + ">** as they've been stood in the server for 7 days without verifying.") //verify-logs
+            console.log(member)
         }
     })
 }
