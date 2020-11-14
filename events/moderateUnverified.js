b2d9b68067394b44d41aeb52c1d2f9770169faa4
Rodrigo <rodrigoleitao25@gmail.com>
2020-11-14 20:20:11 +0000

    Change vars, fix id and 20:22

--- events/moderateUnverified.js
+++ events/moderateUnverified.js
@@ -5,24 +5,24 @@ const fetch = require("node-fetch");
 module.exports = {
     execute(client) {
         var d = new Date()
-        var m = d.getUTCHours()
-        var n = d.getUTCMinutes()
-        if (n == "7" && m == "19") {
+        var h = d.getUTCHours()
+        var m = d.getUTCMinutes()
+        if (h == "20" && m == "21") {
             check(client)
         }
     }
 }
 
 async function check(client) {
-    var timestamp = new Date().getTime() - (7 * 24 * 60 * 60 * 1000)
+    var alert = new Date().getTime() - (7 * 24 * 60 * 60 * 1000)
     //Get date 7 days ago                   d   h    m    s    ms
-    var timestamp2 = new Date().getTime() - (14 * 24 * 60 * 60 * 1000)
+    var kick = new Date().getTime() - (14 * 24 * 60 * 60 * 1000)
     //Get date 14 days ago                   d    h    m    s    ms
 
-    await client.guilds.cache.get().guild.roles.get('415665311828803584').members.forEach(async m => {
-        if (m.joinedAt > timestamp) {
+    await client.guilds.cache.get("549503328472530974").roles.get("739111904672481280").members.forEach(async m => { //guid ID and Unverified
+        if (m.joinedAt > alert) {
             m.send("Hey there!\nWe noticed you haven't verified yourself on our server. Are you having any trouble? Please message Rodry or Stannya or just ask any questions in the verify channel! Otherwise, please send your profile link like shown in the channel.\n\nThis message was sent to you because you have been on our server for too long, and you're in risk of getting kicked for inactivity soon.\nPlease do not reply to this bot.")
-            client.channels.cache.get("662660931838410754").send("Sent an alert to **<@" + m.id + ">** as they've been stood in the server for 7 days without verifying.")
+            client.channels.cache.get("662660931838410754").send("Sent an alert to **<@" + m.id + ">** as they've been stood in the server for 7 days without verifying.") //verify-logs
         }
     })
 }
