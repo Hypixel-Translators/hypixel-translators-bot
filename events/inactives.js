e4eeb4dc0600df4654a8591d86da70f485be4632
Rodrigo <rodrigoleitao25@gmail.com>
2020-11-15 19:04:34 +0000

    lower not higher

--- events/inactives.js
+++ events/inactives.js
@@ -20,9 +20,7 @@ async function check(client) {
     //Get date 14 days ago             d    h    m    s    ms
 
     client.guilds.cache.get("549503328472530974").roles.cache.get("777270531857711114").members.forEach(async member => { //guid ID and Unverified(testing for now)
-        console.log("Joined at " + member.joinedTimestamp)
-        console.log("Alert: " + alert)
-        if (member.joinedTimestamp >= alert) {
+        if (member.joinedTimestamp <= alert) {
             member.send("Hey there!\nWe noticed you haven't verified yourself on our server. Are you having any trouble? Please message Rodry or Stannya or just ask any questions in the verify channel! Otherwise, please send your profile link like shown in the channel.\n\nThis message was sent to you because you have been on our server for too long, and you're in risk of getting kicked for inactivity soon.\nPlease do not reply to this bot.")
             client.channels.cache.get("730042612647723058").send("Sent an alert to **<@" + member.id + ">** as they've been stood in the server for 7 days without verifying.") //verify-logs
             console.log(member.user.tag + "was alerted")
