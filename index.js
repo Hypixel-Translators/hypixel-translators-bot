f72fd9a64a601b573f6f0b49838c19264b5c0984
QkeleQ10 <quintenra@gmail.com>
2020-11-15 21:12:26 +0100

    fetch member applicable only

--- index.js
+++ index.js
@@ -27,9 +27,6 @@ client.once("ready", async () => {
   client.channels.cache.get("775004037443223563").messages.fetch() //htb-language
   const reviewStringsChannels = await client.channels.cache.filter(c => c.name.endsWith("review-strings"))
   reviewStringsChannels.forEach(c => { c.messages.fetch() })
-  client.guilds.cache.get("549503328472530974").members.fetch() // Fetch HTCD
-    .then(() => { console.log("All members fetched!") })
-
   client.user.setStatus("online").catch(console.error)
   client.user.setActivity("+help", { type: "WATCHING" })
 
