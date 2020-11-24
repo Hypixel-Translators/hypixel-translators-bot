8959047d8ff470392ceb0d1561341fe95d35538f
Rodrigo <rodrigoleitao25@gmail.com>
2020-11-24 21:34:04 +0000

    edit ping message

--- commands/ping.js
+++ commands/ping.js
@@ -10,6 +10,7 @@ module.exports = {
   channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
   execute(strings, message) {
     //message.delete();
-    message.channel.send(strings.message.replace("%%pingEmote%%", "<:ping:620954198493888512>").replace("%%ping%%", `${Date.now() - message.createdTimestamp}`).replace("%%latency%%", `${Math.round(message.client.ws.ping)}ms`));
+    message.channel.send(strings.pong);
+    message.edit(strings.message.replace("%%pingEmote%%", "<:ping:620954198493888512>").replace("%%ping%%", `${Date.now() - message.createdTimestamp}`).replace("%%latency%%", `${Math.round(message.client.ws.ping)}ms`));
 }
 };
