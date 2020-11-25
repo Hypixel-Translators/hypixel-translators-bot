1e8f49f8370dae118ac2e9769aef409cf7168434
marzeq <58303665+marzeq@users.noreply.github.com>
2020-11-25 16:08:09 +0100

    Ping command in embeds. (#185)
    * Ping command in embeds.
    The color of the embed depends on the bots` latency.
    
    * Small improvements
    
    * Added comments and consts
    
    Ready for release!
    
    Co-authored-by: QkeleQ10 <quintenra@gmail.com>
    Co-authored-by: Rodrigo <rodrigoleitao25@gmail.com>

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,27 @@ module.exports = {
   cooldown: 60,
   channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
   execute(strings, message) {
-    //message.delete();
-    message.channel.send(strings.message.replace("%%pingEmote%%", "<:ping:620954198493888512>").replace("%%ping%%", `${Date.now() - message.createdTimestamp}`).replace("%%latency%%", `${Math.round(message.client.ws.ping)}ms`));
-}
+    const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
+    const ping = Date.now() - message.createdTimestamp
+    const latency = Math.round(message.client.ws.ping)
+
+    let color;
+    if (ping <= 200) { //if ping is less than 200 the color is green
+      color = successColor
+    } else if (ping <= 400) { //if ping is between 200 and 400 the color is yellow
+      color = workingColor
+    } else if (ping < 0) {
+      color = errorColor
+      console.log("Something went terribly wrong and the ping is negative. Come pick me up I'm scared.")
+    } else { //if ping is higher than 400 the color is red
+      color = errorColor
+    }
+    const embed = new Discord.MessageEmbed()
+      .setColor(color)
+      .setAuthor(strings.moduleName)
+      .setTitle(strings.pong.replace("%%pingEmote%%", "<:ping:620954198493888512>"))
+      .setDescription(strings.message.replace("%%ping%%", ping).replace("%%latency%%", latency))
+      .setFooter(executedBy)
+    message.channel.send(embed)
+  }
 };
