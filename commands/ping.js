b793a8367ad45f600ccc293b2fde23d2f5db4887
marzeq <58303665+marzeq@users.noreply.github.com>
2020-11-25 13:14:34 +0100

    Ping command in embeds. The color of the embed depends on the bots` latency.

--- commands/ping.js
+++ commands/ping.js
@@ -10,6 +10,20 @@ module.exports = {
   channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
   execute(strings, message) {
     //message.delete();
-    message.channel.send(strings.message.replace("%%pingEmote%%", "<:ping:620954198493888512>").replace("%%ping%%", `${Date.now() - message.createdTimestamp}`).replace("%%latency%%", `${Math.round(message.client.ws.ping)}ms`));
+    let color;
+    if (Date.now() - message.createdTimestamp <= 100) {
+      color = successColor;
+    } else if (Date.now() - message.createdTimestamp <= 300) {
+      color = workingColor;
+    } else {
+      color = errorColor;
+    }
+    const embed = new Discord.MessageEmbed()
+                    .setColor(color)
+                    .setAuthor(strings.moduleName)
+                    .setTitle(strings.pong.replace("%%pingEmote%%", "<:ping:620954198493888512>"))
+                    .setDescription(strings.message.replace("%%ping%%", `${Date.now() - message.createdTimestamp}`).replace("%%latency%%", `${Math.round(message.client.ws.ping)}ms`))
+                    // .setFooter(executedBy);
+    message.channel.send(embed);
 }
 };
