8ec9fe0717b8bbc1a56e5401424e4a4a36971d17
Quinten Althues <quintenra@gmail.com>
2020-11-30 16:23:26 +0100

    the `code markdown`

--- commands/help.js
+++ commands/help.js
@@ -138,7 +138,7 @@ async function fetchPage(page, pages, strings, executedBy, madeBy, pageEmbed) {
         .setAuthor(strings.moduleName)
         .setTitle(strings[pages[page].t].replace("%%badge%%", pages[page].b))
         .setFooter(executedBy + " | " + madeBy)
-      pages[page].f.forEach(f => pageEmbed.addFields({ name: strings[f].usage, value: strings[f].description }))
+      pages[page].f.forEach(f => pageEmbed.addFields({ name: `\`${strings[f].usage}\``, value: strings[f].description }))
     } else return console.error("no embed details")
   } else return console.error("no embed listing - internal error")
 
