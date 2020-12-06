371c97a1cc901e1016365b1a2f37980e06f2c891
Rodrigo <rodrigoleitao25@gmail.com>
2020-12-06 22:57:36 +0000

    Removed unused strings

--- commands/invite.js
+++ commands/invite.js
@@ -9,9 +9,8 @@ module.exports = {
   cooldown: 120,
   channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335"],
   execute(strings, message) {
-    const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
     let inviteURL = "https://discord.gg/rcT948A"
-    if (message.guild.premiumTier >= 3) inviteURL = "discord.gg/hypixeltranslators"
+    if (message.guild.premiumTier >= 3) inviteURL = "discord.gg/" + message.guild.vanityURLCode
     message.channel.send(strings.invite.replace("%%invite%%", inviteURL))
   }
 };
