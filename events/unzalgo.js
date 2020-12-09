fec833f3692bddc1423fe39bbd350290ba23ec93
QkeleQ10 <quintenra@gmail.com>
2020-12-09 20:33:50 +0100

    Minify unzalgo.js

--- events/unzalgo.js
+++ events/unzalgo.js
@@ -7,28 +7,19 @@ module.exports = {
         var d = new Date()
         var h = d.getUTCHours()
         var m = d.getUTCMinutes()
-        if ((h == "4" && m == "00") || manual) {
-            check(client)
-        }
+        if ((h == "4" && m == "00") || manual) check(client)
     }
 }
 
 async function check(client) {
     console.log("Checking zalgo nicks now!")
-    await client.guilds.cache.get("549503328472530974").members.fetch()
-        .then(members => {
-            members.forEach(member => {
-                if (isZalgo(member.displayName, 0.1)) {
-                    member.send("Your nickname on the Hypixel Translators Community Discord was automatically set to Unknown because it had Zalgo in it (here's what it looked like before: " + member.displayName + "). If you believe this is an error, feel free to respond to this message saying so. Keep in mind anything sent in these DMs will be sent to staff and that this message was automated.")
-                    member.setNickname("Unknown", "More than 90% of user's name was zalgo")
-                        .then(() => {
-                            console.log("Changed the nick of " + member.user.tag + " to Unknown becase they had Zalgo in their name")
-                        })
-                        .catch(() => {
-                            console.log("Changed the nick of " + member.user.tag + " to Unknown because they had Zalgo in their nick, but couldn't DM them with the alert.")
-                        })
-                }
-            })
-        })
-
+    let members = await client.guilds.cache.get("549503328472530974").members.fetch()
+    members.forEach(member => {
+        if (isZalgo(member.displayName, 0.1)) {
+            member.send("Your nickname on the Hypixel Translators Community Discord was automatically set to Unknown because it had Zalgo in it (here's what it looked like before: " + member.displayName + "). If you believe this is an error, feel free to respond to this message saying so. Keep in mind anything sent in these DMs will be sent to staff and that this message was automated.")
+            member.setNickname("Unknown", "More than 90% of user's name was zalgo")
+                .then(() => console.log("Changed the nick of " + member.user.tag + " to Unknown becase they had Zalgo in their name"))
+                .catch(() => console.log("Changed the nick of " + member.user.tag + " to Unknown because they had Zalgo in their nick, but couldn't DM them with the alert."))
+        }
+    })
 }
