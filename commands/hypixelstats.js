a550862ceea322904382ca373615752edd537dc6
QkeleQ10 <quintenra@gmail.com>
2020-12-14 12:52:58 +0100

    Change some stuff to match bot style and add tip

--- commands/hypixelstats.js
+++ commands/hypixelstats.js
@@ -1,14 +1,14 @@
 const discord = require("discord.js");
 const fetch = require("node-fetch");
-const {loadingColor, errorColor, successColor, neutralColor} = require("../config.json")
+const { loadingColor, errorColor, successColor, neutralColor } = require("../config.json")
 
 
 /*
 PLEASE FOR THE LOVE OF GOD TEST THIS COMMAND.
 I CAN'T TEST IT HERE, AND I CAN ONLY TEST IT BY SETTING UP A NEW EMPTY PROJECT, SO PLEASE PLEASE PLEASE TEST IT
- */
+*/
 
-
+//Credits to marzeq_
 module.exports = {
     name: "hypixelstats",
     description: "Shows you basic Hypixel stats of the provided user.",
@@ -17,46 +17,47 @@ module.exports = {
     cooldown: 60, // feel free to change it
     channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335"], //bots staff-bots bot-dev bot-translators
     allowDM: true,
-    execute (strings, message, args) {
+    execute(strings, message, args) {
         const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
         const madeBy = strings.madeBy.replace("%%QkeleQ10%%", "QkeleQ10#6046")
         let username = args[0]
 
         // make a response to the slothpixel api (hypixel api but we dont need an api key)
-        fetch(`https://api.slothpixel.me/api/players/${username}`, {method: "Get"})
+        fetch(`https://api.slothpixel.me/api/players/${username}`, { method: "Get" })
             .then(res => (res.json())) // get the response json
             .then((json) => { // here we do stuff with the json
-                if (json.error === "Player does not exist") {
-                    throw strings.hypixelApiError
-                }
+
+                if (json.error === "Player does not exist") throw "falseUser"
                 else if (json.error !== undefined) { // if other error we didn't plan for appeared
                     console.log(`Welp, we didn't plan for this to happen. While you have a mental breakdown, enjoy this little error I have for you\n${json.error}`)
+                    throw "apiError"
                 }
+
                 let rank // some ranks are just prefixes so this code accounts for that
-                if (json.prefix !== null) {
-                    rank = json.prefix.replace(/&([1-9]|[a-z])/g, "")
-                }
-                else {
-                    rank = json.rank_formatted.replace(/&([1-9]|[a-z])/g, "")
-                }
+                if (json.prefix !== null) rank = json.prefix.replace(/&([1-9]|[a-z])/g, "")
+                else rank = json.rank_formatted.replace(/&([1-9]|[a-z])/g, "").replace("_PLUS", "+")
                 username = json.username.replace("_", "\\_") // change the nickname in a way that doesn't accidentally mess up the formatting in the embed
                 let language = json.language.toLowerCase().charAt(0).toUpperCase() + json.language.toLowerCase().slice(1) // make the language properly capitalised and not all caps
+                let online
+                if (json.online == true) online = strings.online
+                else online = strings.offline
 
                 // craft the embed and send it
                 const embed = new discord.MessageEmbed()
-                    .setTitle(`${rank} ${json.username}`)
-                    .setDescription(strings.description.replace(/%%username%%/g, username))
+                    .setAuthor(strings.moduleName)
+                    .setTitle(`[${rank}] ${json.username}`)
+                    .setDescription(strings.description.replace("%%username%%", username).replace("%%link%%", "(https://api.slothpixel.me/api/players/%%username%%)"))
                     .addFields(
                         { name: strings.network_level, value: +Math.round(json.level), inline: true },
                         { name: strings.karma, value: json.karma.toLocaleString(), inline: true },
-                        { name: strings.online, value: json.online, inline: true },
+                        { name: online, value: strings.lastGame.replace("%%game%%", json.last_game), inline: true },
                         { name: strings.last_game, value: json.last_game, inline: true },
                         { name: strings.language, value: language, inline: true },
                         { name: strings.uuid, value: json.uuid, inline: true }
 
                     )
-                .setFooter(executedBy + " | " + madeBy, message.author.displayAvatarURL())
+                    .setFooter(executedBy + " | " + madeBy, message.author.displayAvatarURL())
                 message.channel.send(embed)
             })
-    },
-};
+    }
+}
