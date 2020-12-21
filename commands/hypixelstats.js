065fb735779cf230b35caa57f0fb39fdf3e5fa8d
marzeq <58303665+marzeq@users.noreply.github.com>
2020-12-21 14:15:56 +0100

    [New feature] Hypixel stats command. (#199)
    * Hypixel stats command.
    Also included changes to the gitignore to include project files from jetbrains ides`
    
    * Documented the command better so  R O D R Y  knows what it does
    
    * Change some stuff to match bot style and add tip
    
    * remove redundancies and fix command name
    
    * add thumbnail with user skin
    
    * Fixed a few bugs
    
    Will continue working on this tomorrow
    
    * Add support for hat layer
    
    * Ohnonono
    
    * This should be fixed too
    
    * Added color depending on rank, pls test
    
    * Remove last game (redundant)
    
    * Removed the field last game because the information was already provided in online/offline flied
    
    * Fixed some bugs with markdown formatting
    
    * replace all occurences of null with unknown
    
    * Add an alias
    
    * Throw if no user
    
    * Show the absolute level instead of rounded
    
    This was showing levels higher than they actually are and I'm not sure how I can cut the decimals without rounding them so I did this for now
    
    * Add loading message
    
    Did this because slothpixel takes very long to fetch a new player and people could think the bot wasn't working (even I did at some point during testing)
    
    * Change text if user has their last game hidden
    
    * Check if we threw an illegal name at the bot
    
    * Quotes!!
    
    * Fix error in promise not being caught by the error handling system in index.js (implement the error handling system in the command)
    
    * Discord not discord
    
    * d
    
    * im losing my mind
    
    * Last login instead of uuid
    
    * Maybe this will fix the undefined in the error
    
    * Use globalstrings for errors
    
    * Check for some weird edge cases like MonsieurApple
    
    * Fix logging error
    
    * Fixation
    
    * As rodry requested
    
    * uppercase galore!
    
    * Add hypixelstats to +help
    
    * Dynamic last_login last_logout?
    
    * Change one word cuz im picky
    
    Co-authored-by: QkeleQ10 <quintenra@gmail.com>
    Co-authored-by: Rodrigo <rodrigoleitao25@gmail.com>

---
+++ commands/hypixelstats.js
@@ -0,0 +1,124 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const { loadingColor, errorColor, successColor, neutralColor } = require("../config.json")
+
+//Credits to marzeq_
+module.exports = {
+    name: "hypixelstats",
+    description: "Shows you basic Hypixel stats for the provided user.",
+    usage: "+hypixelstats <username>",
+    aliases: ["hstats"],
+    cooldown: 60,
+    channelWhiteList: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335"], //bots staff-bots bot-dev bot-translators
+    allowDM: true,
+    execute(strings, message, args, globalStrings) {
+        function parseColorCode(rank) {
+            let colorCode = rank.substring(1, 2)
+            let colorsJson = { "0": "#000000", "1": "#0000AA", "2": "#00AA00", "3": "#00AAAA", "4": "#AA0000", "5": "#AA00AA", "6": "#FFAA00", "7": "#AAAAAA", "8": "#555555", "9": "#5555FF", "a": "#55FF55", "b": "#55FFFF", "c": "#FF5555", "d": "#FF55FF", "e": "#FFFF55", "f": "#FFFFFF" }
+            return colorsJson[colorCode];
+        }
+
+
+        const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
+        let username = args[0]
+        if (!args[0]) throw "noUser"
+
+        const loadingEmbed = new Discord.MessageEmbed()
+            .setColor(loadingColor)
+            .setAuthor(strings.moduleName)
+            .setTitle(strings.loading)
+            .setDescription(strings.loadingModule)
+            .setFooter(executedBy, message.author.displayAvatarURL())
+        message.channel.send(loadingEmbed)
+            .then(msg => {
+                // make a response to the slothpixel api (hypixel api but we dont need an api key)
+                fetch(`https://api.slothpixel.me/api/players/${username}`, { method: "Get" })
+                    .then(res => (res.json())) // get the response json
+                    .then((json) => { // here we do stuff with the json
+
+                        if (json.error === "Player does not exist" || json.error === "Invalid username or UUID!") throw "falseUser"
+                        else if (json.error !== undefined || json.username === null) { // if other error we didn't plan for appeared
+                            let error
+                            if (json.error === undefined && json.username === null) error = "The user doesn't have a username for some reason"
+                            else if (json.error !== undefined) error = json.error
+                            console.log("Welp, we didn't plan for this to happen. While you have a mental breakdown, enjoy this little error I have for you\n" + error)
+                            throw "apiError"
+                        }
+
+                        let rank // some ranks are just prefixes so this code accounts for that
+                        let color
+                        if (json.prefix !== null) {
+                            color = parseColorCode(json.prefix)
+                            rank = json.prefix.replace(/&([0-9]|[a-z])/g, "")
+                        }
+                        else {
+                            color = parseColorCode(json.rank_formatted)
+                            rank = json.rank_formatted.replace(/&([0-9]|[a-z])/g, "")
+                        }
+                        username = json.username.split("_").join("\\_") // change the nickname in a way that doesn't accidentally mess up the formatting in the embed
+                        let language = json.language.toLowerCase().charAt(0).toUpperCase() + json.language.toLowerCase().slice(1) // make the language properly capitalised and not all caps
+                        language = language.replace("_", " ")
+                        let lang = language
+                        const languageFormat = language.split(" ")
+                        if (languageFormat[1]) {
+                            let language2 = languageFormat[1].charAt(0).toUpperCase() + languageFormat[1].slice(1)
+                            if (languageFormat[1].length === 2) {
+                                language2 = languageFormat[1].toUpperCase()
+                            }
+                            lang = `${languageFormat[0]} ${language2}`
+                        }
+                        let online
+                        if (json.online == true) online = strings.online
+                        else online = strings.offline
+
+                        let linkDiscord
+                        if (json.links.DISCORD === null) linkDiscord = "Not connected"
+                        else linkDiscord = json.links.DISCORD
+
+                        var last_seen
+                        if (json.last_game === null) last_seen = strings.lastGameHidden
+                        else last_seen = strings.lastSeen.replace("%%game%%", json.last_game.replace(/([A-Z]+)/g, ' $1').trim())
+
+                        let lastLoginSelector
+                        if (json.online) { lastLoginSelector = "last_login" } else { lastLoginSelector = "last_logout" }
+                        let lastLogin
+
+                        if (json[lastLoginSelector] !== null) lastLogin = new Date(json[lastLoginSelector]).toLocaleString(strings.dateLocale, { year: 'numeric', month: 'long', day: 'numeric', hour: "2-digit", minute: "2-digit", timeZone: strings.timeZone, timeZoneName: "short" })
+                        else lastLogin = strings.lastLoginHidden
+
+
+                        for (const [key, value] of Object.entries(json)) {
+                            if (value === null) json[key] = strings.unknown
+                        }
+
+                        // craft the embed and send it
+                        const embed = new Discord.MessageEmbed()
+                            .setAuthor(strings.moduleName)
+                            .setTitle(rank + " " + username)
+                            .setDescription(strings.description.replace("%%username%%", username).replace("%%link%%", `(https://api.slothpixel.me/api/players/${username})`))
+                            .addFields(
+                                { name: strings.networkLevel, value: Math.abs(json.level), inline: true },
+                                { name: strings.karma, value: json.karma.toLocaleString(), inline: true },
+                                { name: online, value: last_seen, inline: true },
+                                { name: strings.language, value: lang, inline: true },
+                                { name: strings.discord, value: linkDiscord, inline: true },
+                                { name: strings[lastLoginSelector], value: lastLogin, inline: true }
+
+                            )
+                            .setColor(color)
+                            .setFooter(executedBy, message.author.displayAvatarURL())
+                            .setThumbnail("https://crafatar.com/renders/body/" + json.uuid + "?overlay")
+                        msg.edit(embed)
+                    })
+                    .catch((error) => {
+                        const embed = new Discord.MessageEmbed()
+                            .setColor(errorColor)
+                            .setAuthor(globalStrings.error)
+                            .setTitle(globalStrings[error] || error)
+                            .setFooter(executedBy, message.author.displayAvatarURL())
+
+                        msg.edit(embed)
+                    })
+            })
+    }
+}
