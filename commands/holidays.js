a9fafe243d27fff4836c425b9b3fcec5a1be6bae
Rodrigo <rodrigoleitao25@gmail.com>
2020-12-26 14:57:09 +0000

    Fix holidays cmd and remove duplicate languages

--- commands/holidays.js
+++ commands/holidays.js
@@ -12,25 +12,32 @@ module.exports = {
     channelWhiteList: ["730042612647723058", "551693960913879071"], // bot-development admin-bots
     execute(message, strings, args) {
         if (!message.member.hasPermission("ADMINISTRATOR")) return "noAccess"
+        if (!args[0]) return message.channel.send("You absolute buffoon you need to tell me what holiday to look for.")
+        let holidayName = args[0].toLowerCase()
+        if (holidayName === "newyear" || args.join("").toLowerCase() === "newyear") holidayName = "newYear"
+        if (!strings[holidayName]) return message.channel.send("I have no idea what holiday that is sir.")
         strings = require(`../strings/en/holidays.json`)
-        const stringsFolder = "../strings"
-        const dirPath = path.join(__dirname, stringsFolder)
-        const holidayName = args[0].toLowerCase()
-        if (!args[0]) return message.channel.send("You absolute bafoon you need to tell me what holiday to look for.")
+        const dirPath = path.join(__dirname, "../strings")
         let holiday = []
+        let log = []
         holiday.push(strings[holidayName])
         fs.readdir(dirPath, function (err, files) {
             files.forEach(file => {
                 if (err) return console.error('Unable to scan directory: ' + err)
                 strings = require(`../strings/${file}/holidays.json`)
-                if (!strings[holidayName]) return message.channel.send("I have no idea what holiday that is sir.")
-                if (!holiday.includes(strings[holidayName])) holiday.push(strings[holidayName])
+                if (!holiday.includes(strings[holidayName])) {
+                    holiday.push(strings[holidayName])
+                    log.push(`${file}: ${strings[holidayName]}`)
+                }
             })
             const announcement = holiday.join(" ")
-            message.client.channels.cache.get("549503985501995011").send(`${announcement}\n\n - From the Hypixel Translators Team. ❤`) //announcements
-            .then(msg =>  fetch(`https://discordapp.com/api/v8/channels/549503985501995011/messages/${msg.id}/crosspost`, { method: "Post", headers: { "Authorization": `Bot ${process.env.TOKEN}`} }))
-            message.channel.send(`${holidayName.charAt(0).toUpperCase() + holidayName.slice(1)} announcement sent!`)
-            console.log(`Sent the ${holidayName.charAt(0).toUpperCase() + holidayName.slice(1)} announcement`)
+            if (announcement) {
+                message.client.channels.cache.get("549503985501995011").send(`${announcement}\n\n - From the Hypixel Translators Team. ❤`) //announcements
+                    .then(msg => fetch(`https://discordapp.com/api/v8/channels/549503985501995011/messages/${msg.id}/crosspost`, { method: "Post", headers: { "Authorization": `Bot ${process.env.TOKEN}` } }))
+                message.channel.send(`${holidayName.charAt(0).toUpperCase() + holidayName.slice(1)} announcement sent! Here's each language's translation:\n${log.join("\n")}`)
+                console.log(log)
+                console.log(`Sent the ${holidayName.charAt(0).toUpperCase() + holidayName.slice(1)} announcement`)
+            } else return message.channel.send("For some reason there is nothing in the announcement so I can't send it. Fix your code bro.")
         })
     }
 }
