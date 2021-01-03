a1823589acfa6ed9554bbe7c6d043eaf0fab5f77
Rodrigo <rodrigoleitao25@gmail.com>
2021-01-03 17:10:14 +0000

    Fully release the command after translations are merged

--- commands/translate.js
+++ commands/translate.js
@@ -8,7 +8,6 @@ module.exports = {
     aliases: ["botproject", "translatebot", "bot"],
     cooldown: 120,
     allowDM: true,
-    dev: true,
     channelWhitelist: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
     execute(message, strings) {
         const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
