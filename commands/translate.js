6a2c7a8f5109a31985dd7019a5285e9ae53ee0f0
Rodrigo <rodrigoleitao25@gmail.com>
2021-01-11 23:02:22 +0000

    Disable +translate for the time being

--- commands/translate.js
+++ commands/translate.js
@@ -10,6 +10,7 @@ module.exports = {
     allowDM: true,
     channelWhitelist: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
     execute(message, strings) {
+        throw "disabled"
         const executedBy = strings.executedBy.replace("%%user%%", message.author.tag)
         if (message.channel.type !== "dm" && message.member.roles.cache.find(role => role.name.startsWith("Bot ") && role.id !== "732615152246980628")) {
             const embed = new Discord.MessageEmbed()
