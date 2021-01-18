dbd98daba22484d0aec1233ba24a2ba2a0fafe5a
Rodrigo <rodrigoleitao25@gmail.com>
2021-01-18 20:06:16 +0000

    Add alias

--- commands/hypixel.js
+++ commands/hypixel.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "hypixel",
   description: "Gives you useful information regarding the Hypixel Crowdin project.",
   usage: "+hypixel",
+  aliases: ["hp"],
   cooldown: 120,
   allowDM: true,
   channelWhitelist: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
