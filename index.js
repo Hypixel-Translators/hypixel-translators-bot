e4799e27cb2bf384467047a75b2e091ad9f010aa
Rodrigo <rodrigoleitao25@gmail.com>
2021-01-25 01:21:05 +0000

    Fix enpt bypassing link correction

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ client.on("message", async message => {
 
   //Link correction system
   if (message.content.toLowerCase().includes("/translate/hypixel/") && message.content.includes("://")) if (message.channel.id === "549503328472530976" || message.channel.id === "627594632779399195") { // hypixel translators and proofreaders
-    const msgTxt = message.content.replace(/translate\.hypixel\.net/gi, "crowdin.com").replace(/\/en-(?!en)[a-z]{2,4}/gi, "/en-en")
+    const msgTxt = message.content.replace(/translate\.hypixel\.net/gi, "crowdin.com").replace(/\/en-(?!en#)[a-z]{2,4}/gi, "/en-en")
     if (message.content !== msgTxt) {
       message.react(notAllowed)
       const embed = new Discord.MessageEmbed()
