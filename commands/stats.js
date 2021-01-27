47ae5541eced7d2283f3bda3236370f0bd34feb4
Rodrigo <rodrigoleitao25@gmail.com>
2021-01-27 23:54:53 +0000

    Add channel links to stats

--- commands/stats.js
+++ commands/stats.js
@@ -18,27 +18,33 @@ module.exports = {
                         .setColor(successColor)
                         .setAuthor(strings.moduleName)
                         .setTitle(strings.done)
+                        .setDescription(strings.checkOutAll.replace("%%channel%%", message.guild.channels.cache.find(c => c.name === "hypixel-language-status")).replace("%%channel%%", message.guild.channels.cache.find(c => c.name === "sba-language-status")).replace("%%channel%%", message.guild.channels.cache.find(c => c.name === "bot-language-status")).replace("%%channel%%", message.guild.channels.cache.find(c => c.name === "quickplay-language-status")))
                         .setFooter(executedBy, message.author.displayAvatarURL())
                     message.channel.send(allEmbed)
                 })
                 .catch(err => { throw err })
         } else if (args[0]) {
             let project = args[0].toLowerCase()
+            let channel
             if (project === "hypixel" || project === "hp") {
                 await hypixel(client)
                 project = "Hypixel"
+                channel = "hypixel"
             }
             else if (project === "quickplay" || project === "qp") {
                 await quickplay(client)
                 project = "Quickplay"
+                channel = "quickplay"
             }
             else if (project === "skyblockaddons" || project === "sba") {
                 await skyblockaddons(client)
                 project = "SkyblockAddons"
+                channel = "sba"
             }
             else if (project === "bot") {
                 await bot(client)
                 project = "Bot"
+                channel = "bot"
             }
             else {
                 const errorEmbed = new Discord.MessageEmbed()
@@ -53,6 +59,7 @@ module.exports = {
                 .setColor(successColor)
                 .setAuthor(strings.moduleName)
                 .setTitle(strings.doneProject.replace("%%project%%", project))
+                .setDescription(strings.checkOutOne.replace("%%channel%%", message.guild.channels.cache.find(c => c.name === `${channel}-language-status`)))
                 .setFooter(executedBy, message.author.displayAvatarURL())
             message.channel.send(projectEmbed)
             console.log(`Manually updated the ${project} language statistics.`)
