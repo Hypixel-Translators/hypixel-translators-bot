aae927d5aa591b7c5b6a556ca714844148128a2c
Rodrigo <rodrigoleitao25@gmail.com>
2021-01-30 00:50:31 +0000

    Add sba thread to the sba command

--- commands/Info/skyblockaddons.js
+++ commands/Info/skyblockaddons.js
@@ -8,6 +8,6 @@ module.exports = {
   channelWhitelist: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
   execute(message, strings) {
     //message.delete()
-    message.channel.send(strings.sbaCrowdin.replace("%%crowdin%%", "<https://crowdin.com/project/skyblockaddons>") + "\n" + strings.sbaDiscord.replace("%%discord%%", "<https://discord.gg/biscuit>"))
+    message.channel.send(`${strings.sbaCrowdin.replace("%%crowdin%%", "<https://crowdin.com/project/skyblockaddons>")}\n${strings.sbaThread.replace("%%thread%%", "<https://hypixel.net/threads/2109217/>")}\n${strings.sbaDiscord.replace("%%discord%%", "<https://discord.gg/biscuit>")}`)
   }
 }
