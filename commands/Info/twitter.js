fc488cf987b40d8e7214667ac79b43c5d0208a56
Rodrigo <rodrigoleitao25@gmail.com>
2021-02-03 23:38:55 +0000

    :bulb: Remove useless comments

--- commands/Info/twitter.js
+++ commands/Info/twitter.js
@@ -6,7 +6,6 @@ module.exports = {
   allowDM: true,
   channelWhitelist: ["549894938712866816", "624881429834366986", "730042612647723058", "749391414600925335", "551693960913879071"], // bots staff-bots bot-development bot-translators admin-bots
   execute(message, strings) {
-    //message.delete()
     message.channel.send(strings.twitter.replace("%%twitter%%", "<https://twitter.com/HTranslators>"))
   }
 }
