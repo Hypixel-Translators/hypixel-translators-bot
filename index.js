632b8e870b5dfc613e15784920ef80d2726c6a96
Rodrigo <rodrigoleitao25@gmail.com>
2021-02-07 00:50:13 +0000

    Add async to index and fix mongodb init

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 require("dotenv").config()
 
-//Import commands and events
-const imports = require("./lib/imports")
-imports.setup(client)
-
-//Start MongoDB
-client.mongodb = require('./lib/mongodb')
-client.mongodb.init()
-
 //Export the client
 module.exports.client = client
 
+async function start() {
+    //Import commands and events
+    const imports = require("./lib/imports")
+    imports.setup(client)
+
+    //Start MongoDB
+    client.mongodb = require('./lib/mongodb')
+    await client.mongodb.init()
+}
+
 //Log in
 client.login(process.env.DISCORD_TOKEN)
+
+start()
