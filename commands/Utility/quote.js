8cbb01e5d91495e384f284aa17f5a88fbc2be4d9
Rodrigo <rodrigoleitao25@gmail.com>
2021-02-07 15:10:17 +0000

    Fix stuffs

--- commands/Utility/quote.js
+++ commands/Utility/quote.js
@@ -59,6 +59,7 @@ async function findQuote(executedBy, message, strings, args, collection) {
     let quoteId
     if (!args[0]) quoteId = Math.ceil(Math.random() * Math.floor(all.length)) //generate random id if no arg is given
     else quoteId = Number(args[0])
+    console.log(`Quote with ID ${quoteId} was requested`)
 
     const quote = await collection.findOne({ id: quoteId })
     if (!quote) {
