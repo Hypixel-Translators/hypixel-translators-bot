fd490315ffaa906fcb268467af00e62a1568efc0
Rodrigo <rodrigoleitao25@gmail.com>
2021-02-28 12:01:56 +0000

    Fix errors, rename vars and clean code

--- src/events/stats.ts
+++ src/events/stats.ts
@@ -35,31 +35,29 @@ module.exports = {
 
 async function hypixel(client: HTBClient) {
     const langdb = await client.db.collection("langdb").find().toArray()
-    const url = `https://api.crowdin.com/api/v2/projects/128098/languages/progress?limit=500`
-    const settings = { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } }
-    let index = 0
-    fetch(url, settings)
+    fetch("https://api.crowdin.com/api/v2/projects/128098/languages/progress?limit=500", { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } })
         .then(res => res.json())
         .then(json => {
             const langStatus: LanguageStatus[] = json.data.map((status: LanguageStatus) => {
                 status.data.language = langdb.find(l => l.code === status.data.languageId || l.id === status.data.languageId)
                 return status
             })
-            const reversed = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
+            const sortedSatus = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
                 return nextStatus.data.language.name.localeCompare(currentStatus.data.language.name)
             })
             const channel = client.channels.cache.find(channel => (channel as Discord.TextChannel).name === "hypixel-language-status") as Discord.TextChannel
             channel.messages.fetch()
                 .then((messages: Discord.Collection<Discord.Snowflake, Discord.Message>) => {
-                    const fiMessages = messages.filter(msg => msg.author.bot)
+                    let index = 0
+                    const fiMessages = messages.filter(msg => msg.author === client.user)
                     fiMessages.forEach(msg => {
-                        let r = reversed[index].data
+                        const langData = sortedSatus[index].data
 
                         const embed = new Discord.MessageEmbed()
-                            .setColor(r.language.colour!)
-                            .setTitle(r.language.emoji + " | " + r.language.name || "<:icon_question:756582065834688662>" + " | " + r.language.name)
-                            .setThumbnail("https://crowdin.com/images/flags/" + r.languageId + ".png")
-                            .setDescription(`**${r.translationProgress}% translated (${r.phrases.translated}/${r.phrases.total} strings)**\n${r.approvalProgress}% approved (${r.phrases.approved}/${r.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/hypixel/all/en-${r.language.code}`)
+                            .setColor(langData.language.colour!)
+                            .setTitle(`${langData.language.emoji} | ${langData.language.name}` || "<:icon_question:756582065834688662>" + " | " + langData.language.name)
+                            .setThumbnail("https://crowdin.com/images/flags/" + langData.languageId + ".png")
+                            .setDescription(`**${langData.translationProgress}% translated (${langData.phrases.translated}/${langData.phrases.total} strings)**\n${langData.approvalProgress}% approved (${langData.phrases.approved}/${langData.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/hypixel/all/en-${langData.language.code}`)
                             .setTimestamp()
                         msg.edit("", embed)
                         index++
@@ -68,7 +66,7 @@ async function hypixel(client: HTBClient) {
             const botDev = client.channels.cache.get("730042612647723058") as Discord.TextChannel
             botDev.messages.fetch("782637177552240661") //bot development Hypixel string count
                 .then((stringCount: Discord.Message) => {
-                    if (stringCount.content != langStatus[0].data.phrases.total) {
+                    if (Number(stringCount.content) != langStatus[0].data.phrases.total) {
                         const hypixelTranslators = client.channels.cache.get("549503328472530976") as Discord.TextChannel
                         const stringDiff = Math.abs(Number(Number(langStatus[0].data.phrases.total) - Number(stringCount.content)))
                         if (Number(stringCount.content) < langStatus[0].data.phrases.total) {
@@ -86,36 +84,34 @@ async function hypixel(client: HTBClient) {
 
 async function quickplay(client: HTBClient) {
     const langdb = await client.db.collection("langdb").find().toArray()
-    const url = `https://api.crowdin.com/api/v2/projects/369653/languages/progress?limit=500`
-    const settings = { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } }
-    let index = 0
-    fetch(url, settings)
+    fetch("https://api.crowdin.com/api/v2/projects/369653/languages/progress?limit=500", { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } })
         .then(res => res.json())
         .then(json => {
             const langStatus: LanguageStatus[] = json.data.map((status: LanguageStatus) => {
                 status.data.language = langdb.find((l: LangDbEntry) => l.code === status.data.languageId || l.id === status.data.languageId)
                 return status
             })
-            const reversed = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
+            const sortedStatus = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
                 return nextStatus.data.language.name.localeCompare(currentStatus.data.language.name)
             })
             const channel = client.channels.cache.find(channel => (channel as Discord.TextChannel).name === "quickplay-language-status") as Discord.TextChannel
             channel.messages.fetch()
                 .then((messages: Discord.Collection<Discord.Snowflake, Discord.Message>) => {
-                    const fiMessages = messages.filter((msg: Discord.Message) => msg.author.bot)
+                    let index = 0
+                    const fiMessages = messages.filter(msg => msg.author === client.user)
                     fiMessages.forEach((msg: Discord.Message) => {
-                        let r = reversed[index].data
+                        const langData = sortedStatus[index].data
 
                         let adapColour: string
-                        if (r.approvalProgress > 89) adapColour = successColor
-                        else if (r.approvalProgress > 49) adapColour = loadingColor
+                        if (langData.approvalProgress > 89) adapColour = successColor
+                        else if (langData.approvalProgress > 49) adapColour = loadingColor
                         else adapColour = errorColor
 
                         const embed = new Discord.MessageEmbed()
                             .setColor(adapColour)
-                            .setTitle(r.language.emoji + " | " + r.language.name || "<:icon_question:756582065834688662>" + " | " + r.language.name)
-                            .setThumbnail("https://crowdin.com/images/flags/" + r.languageId + ".png")
-                            .setDescription(`**${r.translationProgress}% translated (${r.phrases.translated}/${r.phrases.total} strings)**\n${r.approvalProgress}% approved (${r.phrases.approved}/${r.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/quickplay/all/en-${r.language.code}`)
+                            .setTitle(`${langData.language.emoji} | ${langData.language.name}` || "<:icon_question:756582065834688662>" + " | " + langData.language.name)
+                            .setThumbnail("https://crowdin.com/images/flags/" + langData.languageId + ".png")
+                            .setDescription(`**${langData.translationProgress}% translated (${langData.phrases.translated}/${langData.phrases.total} strings)**\n${langData.approvalProgress}% approved (${langData.phrases.approved}/${langData.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/quickplay/all/en-${langData.language.code}`)
                             .setTimestamp()
                         msg.edit("", embed)
                         index++
@@ -124,7 +120,7 @@ async function quickplay(client: HTBClient) {
             const botDev = client.channels.cache.get("730042612647723058") as Discord.TextChannel
             botDev.messages.fetch("782637234322931733") //bot-development Quickplay string count
                 .then((stringCount: Discord.Message) => {
-                    if (stringCount.content != langStatus[0].data.phrases.total) {
+                    if (Number(stringCount.content) != langStatus[0].data.phrases.total) {
                         const qpTranslators = client.channels.cache.get("646383292010070016") as Discord.TextChannel
                         const stringDiff = Math.abs(Number(Number(langStatus[0].data.phrases.total) - Number(stringCount.content)))
                         if (Number(stringCount.content) < langStatus[0].data.phrases.total) {
@@ -142,36 +138,34 @@ async function quickplay(client: HTBClient) {
 
 async function bot(client: HTBClient) {
     const langdb = await client.db.collection("langdb").find().toArray()
-    const url = `https://api.crowdin.com/api/v2/projects/436418/languages/progress?limit=500`
-    const settings = { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } }
-    let index = 0
-    fetch(url, settings)
+    fetch("https://api.crowdin.com/api/v2/projects/436418/languages/progress?limit=500", { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } })
         .then(res => res.json())
         .then(json => {
             const langStatus: LanguageStatus[] = json.data.map((status: LanguageStatus) => {
                 status.data.language = langdb.find(l => l.code === status.data.languageId || l.id === status.data.languageId)
                 return status
             })
-            const reversed = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
+            const sortedStatus = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
                 return nextStatus.data.language.name.localeCompare(currentStatus.data.language.name)
             })
             const channel = client.channels.cache.find(channel => (channel as Discord.TextChannel).name === "bot-language-status") as Discord.TextChannel
             channel.messages.fetch()
                 .then((messages: Discord.Collection<Discord.Snowflake, Discord.Message>) => {
-                    const fiMessages = messages.filter(msg => msg.author.bot)
+                    let index = 0
+                    const fiMessages = messages.filter(msg => msg.author === client.user)
                     fiMessages.forEach(msg => {
-                        let r = reversed[index].data
+                        const langData = sortedStatus[index].data
 
                         let adapColour: string
-                        if (r.approvalProgress > 89) adapColour = successColor
-                        else if (r.approvalProgress > 49) adapColour = loadingColor
+                        if (langData.approvalProgress > 89) adapColour = successColor
+                        else if (langData.approvalProgress > 49) adapColour = loadingColor
                         else adapColour = errorColor
 
                         const embed = new Discord.MessageEmbed()
                             .setColor(adapColour)
-                            .setTitle(r.language.emoji + " | " + r.language.name || "<:icon_question:756582065834688662>" + " | " + r.language.name)
-                            .setThumbnail((r.language.flag))
-                            .setDescription(`**${r.translationProgress}% translated (${r.phrases.translated}/${r.phrases.total} strings)**\n${r.approvalProgress}% approved (${r.phrases.approved}/${r.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/hypixel-translators-bot/all/en-${r.language.code}`)
+                            .setTitle(`${langData.language.emoji} | ${langData.language.name}` || "<:icon_question:756582065834688662>" + " | " + langData.language.name)
+                            .setThumbnail((langData.language.flag))
+                            .setDescription(`**${langData.translationProgress}% translated (${langData.phrases.translated}/${langData.phrases.total} strings)**\n${langData.approvalProgress}% approved (${langData.phrases.approved}/${langData.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/hypixel-translators-bot/all/en-${langData.language.code}`)
                             .setTimestamp()
                         msg.edit("", embed)
                         index++
@@ -180,7 +174,7 @@ async function bot(client: HTBClient) {
             const botDev = client.channels.cache.get("730042612647723058") as Discord.TextChannel
             botDev.messages.fetch("782637303427497994") //bot-development Bot string count
                 .then((stringCount: Discord.Message) => {
-                    if (stringCount.content != langStatus[0].data.phrases.total) {
+                    if (Number(stringCount.content) != langStatus[0].data.phrases.total) {
                         const botTranslators = client.channels.cache.get("749391414600925335") as Discord.TextChannel
                         const stringDiff = Math.abs(Number(Number(langStatus[0].data.phrases.total) - Number(stringCount.content)))
                         if (Number(stringCount.content) < langStatus[0].data.phrases.total) {
@@ -198,37 +192,35 @@ async function bot(client: HTBClient) {
 
 async function skyblockaddons(client: HTBClient) {
     const langdb = await client.db.collection("langdb").find().toArray()
-    const url = `https://api.crowdin.com/api/v2/projects/369493/languages/progress?limit=500`
-    const settings = { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } }
-    let index = 0
-    fetch(url, settings)
+    fetch("https://api.crowdin.com/api/v2/projects/369493/languages/progress?limit=500", { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + ctokenV2 } })
         .then(res => res.json())
         .then(json => {
             const langStatus: LanguageStatus[] = json.data.map((status: LanguageStatus) => {
                 status.data.language = langdb.find((l: LangDbEntry) => l.code === status.data.languageId || l.id === status.data.languageId)
                 return status
             })
-            const reversed = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
+            const sortedStatus = Array.from(langStatus).sort((currentStatus: LanguageStatus, nextStatus: LanguageStatus) => {
                 return nextStatus.data.language.name.localeCompare(currentStatus.data.language.name)
             })
             const channel = client.channels.cache.find(channel => (channel as Discord.TextChannel).name === "sba-language-status") as Discord.TextChannel
             channel.messages.fetch()
                 .then((messages: Discord.Collection<Discord.Snowflake, Discord.Message>) => {
-                    const fiMessages = messages.filter(msg => msg.author.bot)
+                    let index = 0
+                    const fiMessages = messages.filter(msg => msg.author === client.user)
                     fiMessages.forEach(msg => {
-                        let r = reversed[index].data
+                        const langData = sortedStatus[index].data
 
                         let adapColour: string
-                        if (r.approvalProgress > 89) adapColour = successColor
-                        else if (r.approvalProgress > 49) adapColour = loadingColor
+                        if (langData.approvalProgress > 89) adapColour = successColor
+                        else if (langData.approvalProgress > 49) adapColour = loadingColor
                         else adapColour = errorColor
 
                         const embed = new Discord.MessageEmbed()
                             .setColor(adapColour)
-                            .setDescription(`**${r.translationProgress}% translated (${r.phrases.translated}/${r.phrases.total} strings)**\n${r.approvalProgress}% approved (${r.phrases.approved}/${r.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/skyblockaddons/all/en-${r.language.code}`)
-                            .setThumbnail((r.language.flag))
+                            .setTitle(`${langData.language.emoji} | ${langData.language.name}`)
+                            .setDescription(`**${langData.translationProgress}% translated (${langData.phrases.translated}/${langData.phrases.total} strings)**\n${langData.approvalProgress}% approved (${langData.phrases.approved}/${langData.phrases.total} strings)\n\nTranslate at https://crowdin.com/translate/skyblockaddons/all/en-${langData.language.code}`)
+                            .setThumbnail((langData.language.flag))
                             .setTimestamp()
-                        if (r.language) { embed.setTitle(r.language.emoji + " | " + r.language.name) } else { embed.setTitle("<:icon_question:756582065834688662> | " + r.language!.name) }
                         msg.edit("", embed)
                         index++
                     })
@@ -236,7 +228,7 @@ async function skyblockaddons(client: HTBClient) {
             const botDev = client.channels.cache.get("730042612647723058") as Discord.TextChannel
             botDev.messages.fetch("782637265230626836") //bot-development Sba string count
                 .then((stringCount: Discord.Message) => {
-                    if (stringCount.content != langStatus[0].data.phrases.total) {
+                    if (Number(stringCount.content) != langStatus[0].data.phrases.total) {
                         const sbaTranslators = client.channels.cache.get("748594964476329994") as Discord.TextChannel
                         const stringDiff = Math.abs(Number(Number(langStatus[0].data.phrases.total) - Number(stringCount.content)))
                         if (Number(stringCount.content) < langStatus[0].data.phrases.total) {
