2a7035189d97d1657394e6de7ea66bae04780c6d
Rodrigo <rodrigoleitao25@gmail.com>
2021-03-02 22:38:47 +0000

    Make that bad boy async!

--- commands/Admin/eval.js
+++ commands/Admin/eval.js
@@ -21,8 +21,10 @@ module.exports = {
     const client = message.client
 
     let evaled
+    let code = args.join(" ").replace(/[“”]/gim, '"')
+    if (code.includes("await ")) code = `(async () => {\n${code}\n})();`
     try {
-      evaled = await eval(args.join(" ").replace(/[“”]/gim, '"'))
+      evaled = await eval(code)
       message.channel.send(inspect(evaled).substring(0, 255))
       console.log(inspect(evaled))
     } catch (error) {
