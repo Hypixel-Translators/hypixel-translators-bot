32b20397b6c79b1b4642d6562bd53ea7e9707c88
ImRodry <38259440+ImRodry@users.noreply.github.com>
2021-03-15 23:50:19 +0000

    Check for slowmodes bigger than 6 hours

--- src/commands/Staff/slowmode.ts
+++ src/commands/Staff/slowmode.ts
@@ -9,7 +9,7 @@ const command: Command = {
     async execute(message: Discord.Message, args: string[]) {
         await message.delete()
         if (args[0].toLowerCase() === "off") args[0] = "0"
-        if (!Number(args[0]) && Number(args[0]) < 0 || !(message.channel instanceof Discord.TextChannel)) return
+        if (!Number(args[0]) && Number(args[0]) < 0 || Number(args[0]) > 21600|| !(message.channel instanceof Discord.TextChannel)) return
         message.channel.setRateLimitPerUser(Number(args[0]), `Set by ${message.author.tag}`)
     }
 }
