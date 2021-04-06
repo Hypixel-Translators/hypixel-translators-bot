eee3705a51e19f14d15d21d8ea31bb0a9e0cbab0
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-04-06 14:55:06 +0100

    Fix message.member throwing error in DMs

--- src/commands/Utility/language.ts
+++ src/commands/Utility/language.ts
@@ -15,8 +15,8 @@ const command: Command = {
     cooldown: 5,
     async execute(message: Discord.Message, args: string[], getString: (path: string, variables?: { [key: string]: string | number }, cmd?: string, lang?: string) => any) {
         let executedBy = getString("executedBy", { user: message.author.tag }, "global")
-        const collection = db.collection("users")
-        const stringsFolder = "./strings/"
+        const collection = db.collection("users"),
+            stringsFolder = "./strings/"
 
         if (args[0]) {
             if (args[0] === "add") {
@@ -52,7 +52,7 @@ const command: Command = {
                 const files = fs.readdirSync(stringsFolder)
                 let langList = ""
                 files.forEach(async (element, index, array) => {
-                    if (element === "empty" && !message.member!.roles.cache.has("764442984119795732")) return //Discord Administrator
+                    if (element === "empty" && !message.member?.roles.cache.has("764442984119795732")) return //Discord Administrator
                     let languageString: string
                     if (element === "empty") languageString = "Empty"
                     else languageString = getString(element)
@@ -67,7 +67,7 @@ const command: Command = {
                         await message.channel.send(embed)
                     }
                 })
-            } else if (args[0] === "stats" && message.member!.roles.cache.has("764442984119795732")) { //Discord Administrator
+            } else if (args[0] === "stats" && message.member?.roles.cache.has("764442984119795732")) { //Discord Administrator
                 if (!args[1]) throw "noLang"
                 const files = fs.readdirSync(stringsFolder)
                 if (!files.includes(args[1])) throw "falseLang"
@@ -89,7 +89,7 @@ const command: Command = {
                 const langdb = await db.collection("langdb").find().toArray()
                 const langdbEntry = langdb.find(l => l.name.toLowerCase() === newLang)
                 if (langdbEntry) newLang = langdbEntry.code
-                if (newLang === "empty" && !message.member!.roles.cache.has("764442984119795732")) newLang = "denied" //Discord Administrator
+                if (newLang === "empty" && !message.member?.roles.cache.has("764442984119795732")) newLang = "denied" //Discord Administrator
                 const path = `./strings/${newLang}/language.json`
                 fs.access(path, fs.constants.F_OK, async (err) => {
                     if (!err) {
@@ -118,7 +118,7 @@ const command: Command = {
                     } else {
                         await fs.readdir(stringsFolder, async (err, files) => {
                             const emptyIndex = files.indexOf("empty")
-                            if (emptyIndex > -1 && !message.member!.roles.cache.has("764442984119795732")) files.splice(emptyIndex, 1) //Discord Administrator
+                            if (emptyIndex > -1 && !message.member?.roles.cache.has("764442984119795732")) files.splice(emptyIndex, 1) //Discord Administrator
                             const embed = new Discord.MessageEmbed()
                                 .setColor(errorColor)
                                 .setAuthor(getString("moduleName"))
@@ -134,7 +134,7 @@ const command: Command = {
         } else {
             await fs.readdir(stringsFolder, async (err, files) => {
                 const emptyIndex = files.indexOf("empty")
-                if (emptyIndex > -1 && !message.member!.roles.cache.has("764442984119795732")) files.splice(emptyIndex, 1) //Discord Administrator
+                if (emptyIndex > -1 && !message.member?.roles.cache.has("764442984119795732")) files.splice(emptyIndex, 1) //Discord Administrator
                 const embed = new Discord.MessageEmbed()
                     .setColor(neutralColor)
                     .setAuthor(getString("moduleName"))
