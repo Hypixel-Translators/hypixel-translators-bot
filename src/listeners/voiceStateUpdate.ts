2c5c421d991acda77d8e8c12779c00a80524f471
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-04-10 19:21:55 +0100

    Fix server mute log firing for no reason

--- src/listeners/voiceStateUpdate.ts
+++ src/listeners/voiceStateUpdate.ts
@@ -11,7 +11,7 @@ client.on("voiceStateUpdate", (oldState, newState) => {
         if (!oldState.channel && newState.channel) newState.member!.roles.add("829312419406020608", "Joined a voice channel") // In Voice
         else if (oldState.channel && !newState.channel) newState.member!.roles.remove("829312419406020608", "Left a voice channel")
 
-        if (oldState.serverMute != newState.serverMute) {
+        if (!!oldState.serverMute != !!newState.serverMute) { // Convert to falsey value to prevent null != false from triggering the condition
             const embed = new Discord.MessageEmbed()
                 .setColor(newState.serverMute ? errorColor : successColor)
                 .setAuthor(newState.member!.user.tag, newState.member!.user.displayAvatarURL({ format: "png", dynamic: true }))
@@ -19,7 +19,7 @@ client.on("voiceStateUpdate", (oldState, newState) => {
                 .setFooter(`ID: ${newState.member!.id}`)
                 .setTimestamp(Date.now())
             logs.send(embed)
-        } else if (oldState.serverDeaf != newState.serverDeaf) {
+        } else if (!!oldState.serverDeaf != !!newState.serverDeaf) {
             const embed = new Discord.MessageEmbed()
                 .setColor(newState.serverDeaf ? errorColor : successColor)
                 .setAuthor(newState.member!.user.tag, newState.member!.user.displayAvatarURL({ format: "png", dynamic: true }))
