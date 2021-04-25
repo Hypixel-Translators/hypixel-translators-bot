58fbadb38a64d0ad771f2d9fc0689d8f1bc1aa4c
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-04-26 00:00:21 +0100

    Update prefix regex to match prefixes with no space

--- src/commands/Utility/prefix.ts
+++ src/commands/Utility/prefix.ts
@@ -13,7 +13,7 @@ const command: Command = {
   channelWhitelist: ["549894938712866816", "624881429834366986", "730042612647723058"], //bots staff-bots bot-development
   async execute(message: Discord.Message, args: string[], getString: (path: string, variables?: { [key: string]: string | number } | string, cmd?: string, lang?: string) => any) {
     const executedBy = getString("executedBy", { user: message.author.tag }, "global")
-    const nickNoPrefix = message.member!.displayName.replace(/\[[^\s]*\] /g, "")
+    const nickNoPrefix = message.member!.displayName.replace(/\[[^\s]*\] ?/g, "")
     const langdb = await db.collection("langdb").find().toArray()
 
     if (args[0]) {
