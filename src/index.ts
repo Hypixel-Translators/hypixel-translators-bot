aafadec74b6e808a80db3b31f3bce889d8e7a2a9
BanTheNons <59115290+BanTheNons@users.noreply.github.com>
2021-05-10 20:32:11 +0300

    Update src/index.ts
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ export interface Command extends Discord.ApplicationCommandData {
     channelWhitelist?: string[],
     dev?: true,
     category?: string,
-    execute(interaction: Discord.CommandInteraction, lang?: string): any
+    execute(interaction: Discord.CommandInteraction, getString?: (path: string, variables?: { [key: string]: string | number } | string, cmd?: string, lang?: string) => any): any
 }
 
 //Log in
