74988297777cbe3f3d49238b64dc795169468b5d
marzeq <58303665+marzeq@users.noreply.github.com>
2021-05-17 22:52:59 +0200

    Staff messaging confirmation (#322)
    * Staff messaging system confirmation
    
    * Small change
    
    * My brain is absolutely huge
    
    * AAAAAAAAAAAAAĄĄĄĄĄĄĄĄĄĄĄĄĄĄĄĄĄĄĄĄĄ
    
    * Address comments
    
    * Address one change
    
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>
    
    * Change time to 2 days
    
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>
    
    * Address one change
    
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>
    
    * Address comments
    
    * Address commends and add collector on end
    
    * Last polishes, ready for release
    
    Added a duplicated string for Crowdin to copy the translations, will remove after merge
    
    * Make string keys more accurate
    
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>

--- src/lib/dbclient.ts
+++ src/lib/dbclient.ts
@@ -31,7 +31,8 @@ export interface DbUser {
         totalXp: number,
         levelXp: number,
         messageCount: number
-    }
+    },
+    staffMsgTimestamp?: number
 }
 
 export class HTBClient extends Discord.Client {
