0143f4d7171fcca1cf3d907bf12699ecd7d05994
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-06-07 00:21:33 +0100

    Fix a few commands

--- src/commands/Admin/holidays.ts
+++ src/commands/Admin/holidays.ts
@@ -34,27 +34,38 @@ const command: Command = {
     async execute(interaction: Discord.CommandInteraction) {
         let strings = require(`../../../strings/en/holidays.json`)
         const dirPath = path.join(__dirname, "../../../strings"),
-            holidayName = interaction.options.get("holiday")!.value as string
-        let holiday: string[] = []
-        let log: string[] = []
+            holidayName = interaction.options.get("holiday")!.value as string,
+            holiday: string[] = [],
+            log: { [Language: string]: string } = {}
         holiday.push(strings[holidayName])
         fs.readdir(dirPath, async (err, langs) => {
-            if (err) return console.error(`Unable to scan directory.\n${err}`)
+            if (err) return console.error(`Unable to scan directory.\n${err.stack || err}`)
             langs.forEach(lang => {
                 if (lang === "empty") return
-                strings = require(`../../../strings/${lang}/holidays.json`)
+                try {
+                    strings = require(`../../../strings/${lang}/holidays.json`)
+                } catch { }
+                if (!strings) return
                 if (!holiday.includes(strings[holidayName])) {
                     holiday.push(strings[holidayName])
-                    log.push(`${lang}: ${strings[holidayName]}`)
+                    log[lang] = strings[holidayName]
                 }
             })
+            let debugMsg = ""
+            for (const lang in log) {
+                if (!log.hasOwnProperty.call(log, lang)) {
+                    continue;
+                }
+
+                debugMsg = debugMsg.concat(`${lang}: ${log[lang]}\n`)
+            }
             const announcement = holiday.join(" ")
             if (announcement) {
                 const announcements = interaction.client.channels.cache.get("549503985501995011") as Discord.NewsChannel
                 announcements.send(`${announcement}\n\n - From the Hypixel Translators Team. ❤`) //announcements
                     .then(msg => msg.crosspost())
-                await interaction.reply(`${holidayName.charAt(0).toUpperCase() + holidayName.slice(1)} announcement sent! Here's each language's translation:\n${log.join("\n")}`)
-                console.log(log)
+                await interaction.reply(`${holidayName.charAt(0).toUpperCase() + holidayName.slice(1)} announcement sent! Here's each language's translation:\n${debugMsg}`)
+                console.table(log)
                 console.log(`Sent the ${holidayName.charAt(0).toUpperCase() + holidayName.slice(1)} announcement`)
             } else return interaction.reply("For some reason there is nothing in the announcement so I can't send it. Fix your code bro.", { ephemeral: true })
         })
