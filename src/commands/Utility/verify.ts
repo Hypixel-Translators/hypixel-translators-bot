278f720dfecd8da46eecdb4c37a59dd9ee4b6c7a
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-07-15 10:19:29 +0100

    Unset unverifiedTimestamp on verify

--- src/commands/Utility/verify.ts
+++ src/commands/Utility/verify.ts
@@ -35,8 +35,8 @@ const command: Command = {
                 })
             await member.roles.add("569194996964786178", "Manually verified through the command")
             await member.roles.remove("756199836470214848", "Manually verified through the command"); //Add Verified and remove Alerted
-            await db.collection("users").updateOne({ id: member!.id }, { $unset: { unverifiedTimestamp: true } });
-            await db.collection("users").updateOne({ id: member!.id, profile: { $exists: false } }, { $set: { profile: null } })
+            await db.collection("users").updateOne({ id: member.id }, { $unset: { unverifiedTimestamp: true } });
+            await db.collection("users").updateOne({ id: member.id, profile: { $exists: false } }, { $set: { profile: null } })
             await verifyLogs.send(`${interaction.user} manually verified themselves through the command`)
             client.cooldowns.get(this.name)!.delete(interaction.user.id)
             await interaction.reply({ content: "You successfully verified yourself!", ephemeral: true })
@@ -49,17 +49,18 @@ const command: Command = {
             const userDb: DbUser = await client.getUser(interaction.user.id)
             if (userDb.profile || profileUrl && /(https:\/\/)([a-z]{2,}\.)?crowdin\.com\/profile?\/?\S{1,}/gi.test(profileUrl)) {
                 await interaction.defer({ ephemeral: true });
-                await verifyLogs.send(`${interaction.user} was unverified.`)
+                await db.collection("users").updateOne({ id: member.id }, { $unset: { unverifiedTimestamp: true } })
+                await verifyLogs.send(`${interaction.user} is being reverified.`)
                 await crowdinVerify(member, profileUrl?.match(/(https:\/\/)([a-z]{2,}\.)?crowdin\.com\/profile\/\S{1,}/gi)?.[0], true)
                 await interaction.editReply("Your profile has been processed. Check your DMs.")
             } else {
                 await member.roles.remove("569194996964786178", "Unverified") // Verified
-                await db.collection("users").updateOne({ id: member!.id }, { $set: { unverifiedTimestamp: Date.now() } })
+                await db.collection("users").updateOne({ id: member.id }, { $set: { unverifiedTimestamp: Date.now() } })
                 const embed = new Discord.MessageEmbed()
                     .setColor(errorColor as Discord.HexColorString)
                     .setAuthor("Manual verification")
                     .setTitle("You were successfully unverified!")
-                    .setDescription(`Since we didn't have your profile registered on our database, we'd like to ask you to kindly send it to us on the <#569178590697095168> channel. Please make sure your profile is public and that you have your Discord tag (${interaction.user.tag}) in your "About me" section.`)
+                    .setDescription(`Since we didn't have your profile registered on our database, we'd like to ask you to kindly send it to us on the ${verify} channel. Please make sure your profile is public and that you have your Discord tag (${interaction.user.tag}) in your "About me" section.`)
                     .setFooter("Any messages you send here will be sent to staff upon confirmation.")
                 await interaction.user.send({ embeds: [embed] })
                     .then(async () => {
@@ -70,7 +71,7 @@ const command: Command = {
                         embed
                             .setDescription(`Since we didn't have your profile registered on our database, we'd like to ask you to kindly send it to us here. Please make sure your profile is public and that you have your Discord tag (${interaction.user.tag}) in your "About me" section.`)
                             .setFooter("")
-                        await verifyLogs.send(`${interaction.user} was unverified and had DMs off.`)
+                        await verifyLogs.send(`${interaction.user} tried to verify with an invalid profile URL or there was no profile stored for them but they had DMs off so I couldn't tell them.`)
                         await verify.send({ content: `${interaction.user} you had DMs disabled, so here's our message:`, embeds: [embed] })
                             .then(msg => {
                                 setTimeout(() => {
