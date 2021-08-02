2da6097248ef5f10519c18f576aa865f047d44b8
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-08-02 01:09:58 +0100

    Fix lang updater i can't believe this

--- src/lib/crowdinverify.ts
+++ src/lib/crowdinverify.ts
@@ -287,7 +287,7 @@ async function crowdinVerify(member: Discord.GuildMember, url?: string | null, s
 
     //Set the user's language based off of their highest role if this is a manual verification
     if (sendDms) {
-        const highestRole = Object.assign({}, endingMessageProjects).hypixel?.filter(r => r.color).sort((a, b) => b.position - a.position).shift()
+        const highestRole = Object.assign({}, endingMessageProjects).Hypixel?.filter(r => r.color).sort((a, b) => b.position - a.position).shift()
         if (highestRole) {
             const lang = await coll.findOne({ name: highestRole.name.replace(" Translator", "").replace(" Proofreader", "") }) as LangDbEntry | undefined
             if (lang) await db.collection("users").updateOne({ id: member.id, lang: { $eq: "en" } }, { $set: { lang: lang.code } })
