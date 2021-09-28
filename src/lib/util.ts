18cc0b746b07edd408d2785f760cf83c3f5b27b5
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-09-28 21:47:38 +0100

    Remove dumb patch for djs bug

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -74,104 +74,66 @@ export async function updateRoles(member: Discord.GuildMember, json?: GraphQLQue
 		"808032657505255424", //MVP+
 		"808032672160153641", //MVP++
 		"808032689709514852", //YouTuber
-		"551758392339857418", //Hypixel Helper
-		"551758392021090304", //Hypixel Mod
 		"822787676482699297", //Hypixel Game Master
 		"624880339722174464", //Hypixel Admin
 		"715674953697198141"  //Hypixel Staff
 	]
 	if (!json) return await member.roles.remove(roles, "Unverified")
-	let role = member.guild.roles.cache.get("816435344689987585")!, rolesToGive: Discord.Snowflake[] = []
+	let role = member.guild.roles.cache.get("816435344689987585")!
 	switch (json.rank) {
 		case "ADMIN":
-			rolesToGive = ["624880339722174464", "715674953697198141"] // Hypixel Admin and Hypixel Staff
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("624880339722174464"), 1) // Hypixel Admin
 			roles.splice(roles.indexOf("715674953697198141"), 1) // Hypixel Staff
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
+			await member.roles.add(["624880339722174464", "715674953697198141"], `Successfully verified as ${json.username}`) // Hypixel Admin and Hypixel Staff
 			role = member.guild.roles.cache.get("624880339722174464")!
 			break
 		case "GAME_MASTER":
-			rolesToGive = ["822787676482699297", "715674953697198141"] // Hypixel Game Master and Hypixel Staff
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("822787676482699297"), 1) // Hypixel Game Master
 			roles.splice(roles.indexOf("715674953697198141"), 1) // Hypixel Staff
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
-			break
-		case "MODERATOR":
-			rolesToGive = ["551758392021090304", "715674953697198141"] // Hypixel Mod and Hypixel Staff
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
-			roles.splice(roles.indexOf("551758392021090304"), 1) // Hypixel Moderator
-			roles.splice(roles.indexOf("715674953697198141"), 1) // Hypixel Staff
-			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
-			role = member.guild.roles.cache.get("551758392021090304")!
-			break
-		case "HELPER":
-			rolesToGive = ["551758392339857418", "715674953697198141"] // Hypixel Helper and Hypixel Staff
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
-			roles.splice(roles.indexOf("551758392339857418"), 1) // Hypixel Helper
-			roles.splice(roles.indexOf("715674953697198141"), 1) // Hypixel Staff
-			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
-			role = member.guild.roles.cache.get("551758392339857418")!
+			await member.roles.add(["822787676482699297", "715674953697198141"], `Successfully verified as ${json.username}`) // Hypixel Game Master and Hypixel Staff
 			break
 		case "YOUTUBER":
-			rolesToGive = ["808032689709514852"] // YouTuber
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("808032689709514852"), 1) // YouTuber
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
+			await member.roles.add("808032689709514852", `Successfully verified as ${json.username}`) // YouTuber
 			role = member.guild.roles.cache.get("808032689709514852")!
 			break
 		case "MVP_PLUS_PLUS":
-			rolesToGive = ["808032672160153641"] // MVP++
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("808032672160153641"), 1) // MVP++
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
+			await member.roles.add("808032672160153641", `Successfully verified as ${json.username}`) // MVP++
 			role = member.guild.roles.cache.get("808032672160153641")!
 			break
 		case "MVP_PLUS":
-			rolesToGive = ["808032657505255424"] // MVP+
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("808032657505255424"), 1) // MVP+
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
+			await member.roles.add("808032657505255424", `Successfully verified as ${json.username}`) // MVP+
 			role = member.guild.roles.cache.get("808032657505255424")!
 			break
 		case "MVP":
-			rolesToGive = ["808032640631832637"] // MVP
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("808032640631832637"), 1) // MVP
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
+			await member.roles.add("808032640631832637", `Successfully verified as ${json.username}`) // MVP
 			role = member.guild.roles.cache.get("808032640631832637")!
 			break
 		case "VIP_PLUS":
-			rolesToGive = ["808032624215457823"] // VIP+
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("808032624215457823"), 1) // VIP+
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
+			await member.roles.add("808032624215457823", `Successfully verified as ${json.username}`) // VIP+
 			role = member.guild.roles.cache.get("808032624215457823")!
 			break
 		case "VIP":
-			rolesToGive = ["808032608456802337"] // VIP
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("808032608456802337"), 1) // VIP
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add(rolesToGive, `Successfully verified as ${json.username}`)
+			await member.roles.add("808032608456802337", `Successfully verified as ${json.username}`) // VIP
 			role = member.guild.roles.cache.get("808032608456802337")!
 			break
 		default:
-			rolesToGive = ["816435344689987585"] // Unranked
-			member.roles.cache.forEach(r => { if (rolesToGive.includes(r.id)) rolesToGive.splice(rolesToGive.indexOf(r.id), 1) })
 			roles.splice(roles.indexOf("816435344689987585"), 1) // Unranked
 			await member.roles.remove(roles, "Updated roles")
-			await member.roles.add("816435344689987585", `Successfully verified as ${json.username}`)
+			await member.roles.add("816435344689987585", `Successfully verified as ${json.username}`) // Unranked
 			break
 	}
 	return role
