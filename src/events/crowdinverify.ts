8d558395b107e55ae4841dbc3f00fd4c30cc0c6a
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-10-13 00:09:54 +0100

    Fix crowdinverify stopping at unknown user

--- src/events/crowdinverify.ts
+++ src/events/crowdinverify.ts
@@ -12,8 +12,8 @@ export default async function updateVerified(client: HTBClient, manual: boolean,
 		async function verifyUser(n: number) {
 			const user = verifiedUsers[n],
 				member = client.guilds.cache.get(ids.guilds.main)!.members.cache.get(user.id)
-			if (!member) return console.error(`Could not find guild member with ID ${user.id}`)
-			if (user.profile) await crowdinVerify(member, user.profile, false, false)
+			if (!member) console.error(`Could not find guild member with ID ${user.id}`)
+			else if (user.profile) await crowdinVerify(member, user.profile, false, false)
 			if (n + 1 < verifiedUsers.length) await verifyUser(n + 1)
 		}
 		await verifyUser(0)
