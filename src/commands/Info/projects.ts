d84720d3c1861cf7e2a4fd91317eb0ce0c4c0654
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-10-14 22:05:43 +0100

    Fix formatting and command suggestions

--- src/commands/Info/projects.ts
+++ src/commands/Info/projects.ts
@@ -16,13 +16,21 @@ const command: Command = {
 			joinedQuickplay: string,
 			joinedSba: string,
 			joinedBot: string
-		if (member.roles.cache.find(role => role.name === "Hypixel Translator" || role.name === "Hypixel Proofreader" || role.name === "Hypixel Manager")) joinedHypixel = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
+		if (member.roles.cache.find(role => role.name === "Hypixel Translator" || role.name === "Hypixel Proofreader" || role.name === "Hypixel Manager"))
+			joinedHypixel = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
 		else joinedHypixel = `<:vote_no:839262184882044931> **${getString("notJoined")}**`
-		if (member.roles.cache.find(role => role.name === "Quickplay Translator" || role.name === "Quickplay Proofreader" || role.name === "Quickplay Manager")) joinedQuickplay = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
+		if (member.roles.cache.find(role => role.name === "Quickplay Translator" || role.name === "Quickplay Proofreader" || role.name === "Quickplay Manager"))
+			joinedQuickplay = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
 		else joinedQuickplay = `<:vote_no:839262184882044931> **${getString("notJoined")}**`
-		if (member.roles.cache.find(role => role.name === "SkyblockAddons Translator" || role.name === "SkyblockAddons Proofreader" || role.name === "SkyblockAddons Manager")) joinedSba = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
+		if (
+			member.roles.cache.find(
+				role => role.name === "SkyblockAddons Translator" || role.name === "SkyblockAddons Proofreader" || role.name === "SkyblockAddons Manager"
+			)
+		)
+			joinedSba = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
 		else joinedSba = `<:vote_no:839262184882044931> **${getString("notJoined")}**`
-		if (member.roles.cache.find(role => role.name === "Bot Translator" || role.name === "Bot Proofreader" || role.name === "Bot Manager")) joinedBot = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
+		if (member.roles.cache.find(role => role.name === "Bot Translator" || role.name === "Bot Proofreader" || role.name === "Bot Manager"))
+			joinedBot = `<:vote_yes:839262196797669427> **${getString("alreadyJoined")}**`
 		else joinedBot = `<:vote_no:839262184882044931> **${getString("notJoined")}**`
 		const embed = new Discord.MessageEmbed()
 			.setColor(neutralColor as Discord.HexColorString)
@@ -30,10 +38,38 @@ const command: Command = {
 			.setTitle(getString("allProjects"))
 			.setDescription(getString("description"))
 			.addFields(
-				{ name: "Hypixel", value: `${getString("projectInfo", { project: "**Hypixel**", link: "https://crowdin.com/project/hypixel", command: "`/hypixel`" })}\n${joinedHypixel}` },
-				{ name: "Quickplay", value: `${getString("projectInfo", { project: "**Quickplay**", link: "https://crowdin.com/project/quickplay", command: "`/quickplay`" })}\n${joinedQuickplay}` },
-				{ name: "SkyblockAddons", value: `${getString("projectInfo", { project: "**SkyblockAddons**", link: "https://crowdin.com/project/skyblockaddons", command: "`/skyblockaddons`" })}\n${joinedSba}` },
-				{ name: "Hypixel Translators Bot", value: `${getString("projectInfo", { project: "**Hypixel Translators Bot**", link: "https://crowdin.com/project/hypixel-translators-bot", command: "`/bot`" })}\n${joinedBot}` }
+				{
+					name: "Hypixel",
+					value: `${getString("projectInfo", {
+						project: "**Hypixel**",
+						link: "https://crowdin.com/project/hypixel",
+						command: "`/tag hypixel`"
+					})}\n${joinedHypixel}`
+				},
+				{
+					name: "Quickplay",
+					value: `${getString("projectInfo", {
+						project: "**Quickplay**",
+						link: "https://crowdin.com/project/quickplay",
+						command: "`/tag quickplay`"
+					})}\n${joinedQuickplay}`
+				},
+				{
+					name: "SkyblockAddons",
+					value: `${getString("projectInfo", {
+						project: "**SkyblockAddons**",
+						link: "https://crowdin.com/project/skyblockaddons",
+						command: "`/tag skyblockaddons`"
+					})}\n${joinedSba}`
+				},
+				{
+					name: "Hypixel Translators Bot",
+					value: `${getString("projectInfo", {
+						project: "**Hypixel Translators Bot**",
+						link: "https://crowdin.com/project/hypixel-translators-bot",
+						command: "`/tag bot`"
+					})}\n${joinedBot}`
+				}
 			)
 			.setFooter(randomTip, member.displayAvatarURL({ format: "png", dynamic: true }))
 		await interaction.reply({ embeds: [embed] })
