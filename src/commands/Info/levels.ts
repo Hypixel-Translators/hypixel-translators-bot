b21ace7fb1a1e63c8b7211bb94155bf66d6c5d9c
marzeq <58303665+marzeq@users.noreply.github.com>
2021-11-25 22:07:35 +0100

    Apply some suggestions from code review
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>

--- src/commands/Info/levels.ts
+++ src/commands/Info/levels.ts
@@ -121,9 +121,9 @@ function fetchPage(page: number, pages: DbUser[][], getString: GetStringFunction
 		if (pages[page][i].levels) {
 			const totalXp = pages[page][i].levels!.totalXp
 			let formattedXp: string
-			if (totalXp >= 1000000) formattedXp = `${(totalXp / 1000000).toFixed(2)}${getString("million")}`
-			else if (totalXp >= 1000) formattedXp = `${(totalXp / 1000).toFixed(2)}${getString("thousand")}`
-			else formattedXp = totalXp.toString()
+			if (totalXp >= 1_000_000) formattedXp = `${(totalXp / 1_000_000).toFixed(2)}${getString("million")}`
+			else if (totalXp >= 1_000) formattedXp = `${(totalXp / 1_000).toFixed(2)}${getString("thousand")}`
+			else formattedXp = `${totalXp}`
 			pageEmbed.addField(
 				getString("level", {
 					rank: i + 1 + page * 24,
