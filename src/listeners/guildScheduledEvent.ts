2fe1d49e159ebbbb2e5f581777c189eb63352104
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-12-05 21:03:59 +0000

    Fix ending events not removing role

--- src/listeners/guildScheduledEvent.ts
+++ src/listeners/guildScheduledEvent.ts
@@ -1,14 +1,16 @@
 import { client } from "../index"
 import { ids } from "../config.json"
 
-client.on("guildScheduledEventDelete", event => {
-    const role = event.guild!.roles.cache.get(ids.roles.event)!
-    role.members.forEach(async m => await m.roles.remove(role.id, `Event ${event.name} is over`))
-})
-
 client.on("guildScheduledEventUserAdd", async (event, user) => {
     const member = event.guild!.members.cache.get(user.id)
     await member?.roles.add(ids.roles.event, `Showed interest in event ${event.name}`)
+})
+
+client.on("guildScheduledEventUpdate", (oldEvent, newEvent) => {
+    if (oldEvent.status !== newEvent.status && newEvent.status === "COMPLETED") {
+        const role = newEvent.guild!.roles.cache.get(ids.roles.event)!
+        role.members.forEach(async m => await m.roles.remove(role.id, `Event ${newEvent.name} is over`))
+    }
 })
 
 client.on("guildScheduledEventUserRemove", async (event, user) => {
