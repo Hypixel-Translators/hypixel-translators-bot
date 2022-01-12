0e7c19429ab036ae99087ded7eea745f1451d3e4
marzeq <58303665+marzeq@users.noreply.github.com>
2022-01-12 20:06:44 +0100

    Round 2 of adressing comments

--- src/commands/Utility/language.ts
+++ src/commands/Utility/language.ts
@@ -4,7 +4,7 @@ import { GuildMember, MessageEmbed } from "discord.js"
 
 import { colors, ids } from "../../config.json"
 import { db, DbUser } from "../../lib/dbclient"
-import { generateTip, MongoLanguage } from "../../lib/util"
+import { generateTip, MongoLanguage, transformDiscordLocale } from "../../lib/util"
 
 import type { Command, GetStringFunction } from "../../lib/imports"
 
@@ -154,23 +154,24 @@ const command: Command = {
 				}
 			})
 		} else if (subCommand === "reset") {
-			const result = await collection.updateOne({ id: interaction.user.id }, { $unset: { lang: true } })
+			const result = await collection.updateOne({ id: interaction.user.id }, { $unset: { lang: true } }),
+				newLanguage = transformDiscordLocale(interaction.locale) // This is because now that language is not set, we can try to get the locale from discord
 			if (result.modifiedCount) {
-				randomTip = generateTip(getString, language)
+				randomTip = generateTip(getString, newLanguage)
 				const embed = new MessageEmbed({
 					color: colors.success,
-					author: { name: getString("moduleName", this.name, language) },
-					title: getString("resetTitle", this.name, language),
-					description: getString("credits", this.name, language),
+					author: { name: getString("moduleName", this.name, newLanguage) },
+					title: getString("resetTitle", this.name, newLanguage),
+					description: getString("credits", this.name, newLanguage),
 					footer: { text: randomTip, iconURL: (member ?? interaction.user).displayAvatarURL({ format: "png", dynamic: true }) },
 				})
 				await interaction.reply({ embeds: [embed] })
 			} else {
 				const embed = new MessageEmbed({
 					color: colors.error,
-					author: { name: getString("moduleName", this.name, language) },
-					title: getString("didntChange", language),
-					description: getString("notSet", this.name, language),
+					author: { name: getString("moduleName", this.name, newLanguage) },
+					title: getString("didntChange", newLanguage),
+					description: getString("notSet", this.name, newLanguage),
 					footer: { text: randomTip, iconURL: (member ?? interaction.user).displayAvatarURL({ format: "png", dynamic: true }) },
 				})
 				await interaction.reply({ embeds: [embed] })
