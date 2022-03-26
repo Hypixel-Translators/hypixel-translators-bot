18550b27a0433949e3d4838d625c36e6c9e9f9ee
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-03-26 20:23:14 +0000

    feat(events): set event channel perms automatically

--- src/listeners/guildScheduledEvent.ts
+++ src/listeners/guildScheduledEvent.ts
@@ -1,14 +1,20 @@
 import { ids } from "../config.json"
 import { client } from "../index"
 
+import type { VoiceChannel } from "discord.js"
+
 client.on("guildScheduledEventUserAdd", async (event, user) => {
 	await event.guild!.members.cache.get(user.id)?.roles.add(ids.roles.event, `Showed interest in event ${event.name}`)
 })
 
-client.on("guildScheduledEventUpdate", (oldEvent, newEvent) => {
-	if (oldEvent.status !== newEvent.status && newEvent.status === "COMPLETED") {
-		const role = newEvent.guild!.roles.cache.get(ids.roles.event)!
-		role.members.forEach(async m => await m.roles.remove(role.id, `Event ${newEvent.name} is over`))
+client.on("guildScheduledEventUpdate", async (oldEvent, newEvent) => {
+	if (oldEvent.status !== newEvent.status) {
+		const eventChannel = client.channels.cache.get(ids.channels.event) as VoiceChannel
+		if (newEvent.status === "COMPLETED") {
+			const role = newEvent.guild!.roles.cache.get(ids.roles.event)!
+			role.members.forEach(async m => await m.roles.remove(role.id, `${newEvent.name} event is over`))
+			await eventChannel.permissionOverwrites.edit(newEvent.guildId, { CONNECT: false })
+		} else if (newEvent.status === "ACTIVE") await eventChannel.permissionOverwrites.edit(newEvent.guildId, { CONNECT: null })
 	}
 })
 
