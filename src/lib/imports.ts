50f3b7ca4589c601e7042c7151981873fb1858e8
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-04-16 18:44:32 +0100

    refactor: add error handling for all situations

--- src/lib/imports.ts
+++ src/lib/imports.ts
@@ -36,8 +36,16 @@ export function setup(client: HTBClient) {
 				if (!discordLocale || locale === "en") continue
 				try {
 					const commandsJson: CommandStrings = require(`../../strings/${locale}/commands.json`)
-					if (!(command.name in commandsJson.names)) continue // Command is admin or staff only
+					if (["Admin", "Staff"].includes(command.category)) continue
 
+					if (!commandsJson.names[command.name]) {
+						console.error(`Missing name localization for ${command.name} in ${locale}`)
+						continue
+					}
+					if (!commandsJson.descriptions[command.name]) {
+						console.error(`Missing description localization for ${command.name} in ${locale}`)
+						continue
+					}
 					command.nameLocalizations[discordLocale] = commandsJson.names[command.name]
 					command.descriptionLocalizations[discordLocale] = commandsJson.descriptions[command.name]
 
@@ -45,11 +53,29 @@ export function setup(client: HTBClient) {
 						option.nameLocalizations ??= {}
 						option.descriptionLocalizations ??= {}
 
+						if (!optionJson.name) {
+							console.error(`Missing option name localization for ${command.name}'s ${option.name} option in ${locale}`)
+							return
+						}
+						if (!optionJson.description) {
+							console.error(`Missing option description localization for ${command.name}'s ${option.name} option in ${locale}`)
+							return
+						}
 						option.nameLocalizations[discordLocale!] = optionJson.name
 						option.descriptionLocalizations[discordLocale!] = optionJson.description
 
 						if ("choices" in option) {
+							if (!optionJson.choices) {
+								console.error(`Missing option choices localization for ${command.name}'s ${option.name} option in ${locale}`)
+								return
+							}
 							for (const choice of option.choices ?? []) {
+								if (!optionJson.choices![choice.value]) {
+									console.error(
+										`Missing option choice localization for ${command.name}'s ${option.name} option's ${choice.value} choice in ${locale}`,
+									)
+									return
+								}
 								choice.nameLocalizations ??= {}
 								choice.nameLocalizations[discordLocale!] = optionJson.choices![choice.value]
 							}
@@ -59,6 +85,15 @@ export function setup(client: HTBClient) {
 					}
 
 					for (const option of command.options ?? []) {
+						if (!commandsJson.options[command.name]) {
+							console.error(`Missing option localization for ${command.name} in ${locale}`)
+							return
+						}
+						if (!commandsJson.options[command.name][option.name]) {
+							console.error(`Missing option localization for ${command.name}'s ${option.name} option in ${locale}`)
+							return
+						}
+
 						assignLocalisation(option, commandsJson.options[command.name][option.name])
 					}
 				} catch (err) {
