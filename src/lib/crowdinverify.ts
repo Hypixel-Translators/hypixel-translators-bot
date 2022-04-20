c4007de15eea8149fc8b4810041d9dbb23c1f7e5
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-04-20 19:54:51 +0100

    fix(crowdinVerify): correctly set language id

--- src/lib/crowdinverify.ts
+++ src/lib/crowdinverify.ts
@@ -334,7 +334,12 @@ export async function crowdinVerify(member: GuildMember, url?: string | null, se
 			.shift()
 		if (highestRole) {
 			const lang = await languages.findOne({ name: highestRole.name.replace(" Translator", "").replace(" Proofreader", "") })
-			if (lang) await usersColl.updateOne({ id: member.id, $or: [{ lang: "en" }, { lang: { $exists: false } }] }, { $set: { lang: lang.code } })
+			if (lang) {
+				await usersColl.updateOne(
+					{ id: member.id, $or: [{ lang: "en" }, { lang: { $exists: false } }] },
+					{ $set: { lang: lang.id.replace("-", "_") } },
+				)
+			}
 		}
 	}
 
