3503f5d78e7c2647d26c863d396a9bc643e3ce33
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-07-11 01:42:50 +0100

    feat: check for mistakes in translations

--- src/lib/imports.ts
+++ src/lib/imports.ts
@@ -1,10 +1,21 @@
 import { readdirSync, statSync } from "node:fs"
 import { resolve, sep } from "node:path"
 
+import {
+	type ChatInputApplicationCommandData,
+	type Snowflake,
+	type ChatInputCommandInteraction,
+	type ApplicationCommandOptionData,
+	type LocaleString,
+	EmbedBuilder,
+	Routes,
+} from "discord.js"
+
 import { botLocales, transformBotLocale } from "./util"
 
+import { colors, ids } from "../config.json"
+
 import type { HTBClient } from "./dbclient"
-import type { ChatInputApplicationCommandData, Snowflake, ChatInputCommandInteraction, ApplicationCommandOptionData } from "discord.js"
 
 export function findCommands(dir: string, pattern: string) {
 	let results: string[] = []
@@ -19,6 +30,9 @@ export function findCommands(dir: string, pattern: string) {
 	return results
 }
 
+const localizationErrors: { command: string; type: "name" | "option"; locale: LocaleString; string: string }[] = [],
+	nameRegex = /^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/u
+
 export function setup(client: HTBClient) {
 	// Set commands
 	const cmdFiles = findCommands("./dist/commands", ".js")
@@ -31,73 +45,32 @@ export function setup(client: HTBClient) {
 
 			command.nameLocalizations ??= {}
 			command.descriptionLocalizations ??= {}
+
 			for (const locale of botLocales) {
 				const discordLocale = transformBotLocale(locale)
 				if (!discordLocale || locale === "en") continue
 				try {
 					const commandsJson: CommandStrings = require(`../../strings/${locale}/commands.json`)
-					if (["Admin", "Staff"].includes(command.category)) continue
+					if (
+						["Admin", "Staff"].includes(command.category) ||
+						["names", "descriptions", "options"].some(p => !commandsJson[p as keyof CommandStrings][command.name])
+					)
+						continue
 
-					if (!commandsJson.names[command.name]) {
-						console.error(`Missing name localization for ${command.name} in ${locale}`)
+					if (
+						!nameRegex.test(commandsJson.names[command.name]) ||
+						commandsJson.names[command.name] !== commandsJson.names[command.name].toLowerCase()
+					) {
+						localizationErrors.push({ command: command.name, type: "name", locale: discordLocale, string: commandsJson.names[command.name] })
 						continue
 					}
-					if (!commandsJson.descriptions[command.name]) {
-						console.error(`Missing description localization for ${command.name} in ${locale}`)
-						continue
-					}
+
 					command.nameLocalizations[discordLocale] = commandsJson.names[command.name]
 					command.descriptionLocalizations[discordLocale] = commandsJson.descriptions[command.name]
 
-					function assignLocalisation(option: ApplicationCommandOptionData, optionJson: CommandOption) {
-						option.nameLocalizations ??= {}
-						option.descriptionLocalizations ??= {}
-
-						if (!optionJson.name) {
-							console.error(`Missing option name localization for ${command.name}'s ${option.name} option in ${locale}`)
-							return
-						}
-						if (!optionJson.description) {
-							console.error(`Missing option description localization for ${command.name}'s ${option.name} option in ${locale}`)
-							return
-						}
-						option.nameLocalizations[discordLocale!] = optionJson.name
-						option.descriptionLocalizations[discordLocale!] = optionJson.description
-
-						if ("choices" in option) {
-							if (!optionJson.choices) {
-								console.error(`Missing option choices localization for ${command.name}'s ${option.name} option in ${locale}`)
-								return
-							}
-							for (const choice of option.choices ?? []) {
-								if (!optionJson.choices![choice.value]) {
-									console.error(
-										`Missing option choice localization for ${command.name}'s ${option.name} option's ${choice.value} choice in ${locale}`,
-									)
-									return
-								}
-								choice.nameLocalizations ??= {}
-								choice.nameLocalizations[discordLocale!] = optionJson.choices![choice.value]
-							}
-						}
-
-						if ("options" in option) for (const subOption of option.options ?? []) assignLocalisation(subOption, optionJson.options![subOption.name])
-					}
-
-					for (const option of command.options ?? []) {
-						if (!commandsJson.options[command.name]) {
-							console.error(`Missing option localization for ${command.name} in ${locale}`)
-							return
-						}
-						if (!commandsJson.options[command.name][option.name]) {
-							console.error(`Missing option localization for ${command.name}'s ${option.name} option in ${locale}`)
-							return
-						}
-
-						assignLocalisation(option, commandsJson.options[command.name][option.name])
-					}
+					for (const option of command.options ?? []) assignLocalisation(option, commandsJson.options[command.name][option.name], discordLocale)
 				} catch (err) {
-					console.error(`Failed to load command localization for ${locale} for command ${command.name}: ${err}`)
+					console.error(`Failed to load command localization for ${locale} for command ${command.name}:\n`, err)
 					continue
 				}
 			}
@@ -107,11 +80,56 @@ export function setup(client: HTBClient) {
 		console.log(`Loaded ${cmdFiles.length} commands.`)
 	}
 
+	if (localizationErrors.length > 25) {
+		console.error(`There were ${localizationErrors.length} localization errors.`, ...localizationErrors)
+		client.rest.post(Routes.channelMessages(ids.channels.botDev), {
+			body: {
+				content: `There were ${localizationErrors.length} errors in the command localization strings. I couldn't send them all here to check the console for more info.`,
+			},
+		})
+	} else if (localizationErrors.length) {
+		const embed = new EmbedBuilder({
+			color: colors.error,
+			title: "Found errors on command localization strings",
+			fields: localizationErrors.map(err => ({ name: `${err.locale}: ${err.command}: ${err.type}`, value: err.string })),
+			timestamp: Date.now(),
+		})
+		client.rest.post(Routes.channelMessages(ids.channels.botDev), { body: { embeds: [embed.toJSON()] } })
+	}
+
 	// Setup listeners
 	const listeners = readdirSync("./dist/listeners").filter(f => f.endsWith(".js"))
 	if (listeners.length <= 0) return console.log("There are no events to load...")
 	listeners.forEach(file => require(`../listeners/${file}`))
 	console.log(`Loaded ${listeners.length} events.`)
+}
+
+function assignLocalisation(option: ApplicationCommandOptionData, optionJson: CommandOption, locale: LocaleString) {
+	if (!optionJson.name || !optionJson.description) return
+
+	option.nameLocalizations ??= {}
+	option.descriptionLocalizations ??= {}
+
+	if (!nameRegex.test(optionJson.name) || optionJson.name !== optionJson.name.toLowerCase()) {
+		localizationErrors.push({ command: option.name, type: "option", locale, string: optionJson.name })
+		return
+	}
+
+	option.nameLocalizations[locale] = optionJson.name
+	option.descriptionLocalizations[locale] = optionJson.description
+
+	if ("choices" in option) {
+		if (!optionJson.choices) return
+
+		for (const choice of option.choices ?? []) {
+			if (!optionJson.choices[choice.value]) continue
+
+			choice.nameLocalizations ??= {}
+			choice.nameLocalizations[locale] = optionJson.choices[choice.value]
+		}
+	}
+
+	if ("options" in option) for (const subOption of option.options ?? []) assignLocalisation(subOption, optionJson.options![subOption.name], locale)
 }
 
 // Command interface
